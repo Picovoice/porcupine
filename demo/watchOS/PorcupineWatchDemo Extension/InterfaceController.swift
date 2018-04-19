1b1f0eb4b598567764a904fb725d768eb9271557
Dominique <yahyavi10@gmail.com>
2018-04-19 10:15:54 -0500

    linked start stop to porcupine

--- demo/watchOS/PorcupineWatchDemo Extension/InterfaceController.swift
+++ demo/watchOS/PorcupineWatchDemo Extension/InterfaceController.swift
@@ -8,13 +8,14 @@
 
 import WatchKit
 import Foundation
-
+import AVFoundation
 
 class InterfaceController: WKInterfaceController {
 
     @IBOutlet var containerGroup: WKInterfaceGroup!
     @IBOutlet var wakeWordPicker: WKInterfacePicker!
     @IBOutlet var startToggleButton: WKInterfaceButton!
+    var porcupineManager: PorcupineManager!
     var isRecording = false
 
     let wakeWords = [
@@ -26,7 +27,7 @@ class InterfaceController: WKInterfaceController {
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+        initializeAudioSession()
         
         let pickerItems: [WKPickerItem] = wakeWords.map {
             let pickerItem = WKPickerItem()
@@ -45,11 +46,70 @@ class InterfaceController: WKInterfaceController {
 
     @IBAction func startTogglePressed() {
         if isRecording {
+            wakeWordPicker.setEnabled(true)
+            porcupineManager.stop()
             startToggleButton.setTitle("Start")
         } else {
+            wakeWordPicker.setEnabled(false)
+            startPorcupineManager()
             startToggleButton.setTitle("Stop")
         }
         isRecording = !isRecording
+    }
+    
+    
+    private func startPorcupineManager() {
+        print("starting porcupine manager")
+        let modelFilePath = Bundle.main.path(forResource: "porcupine_params", ofType: "pv")
+        let keywordFilePath = Bundle.main.path(forResource: wakeWord.lowercased() + "_ios", ofType: "ppn")
+        
+        let keywordCallback: (() -> Void) = {
+            DispatchQueue.main.async {
+                WKInterfaceDevice.current().play(.start)
+                self.containerGroup.setBackgroundColor(.orange)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+                    self.containerGroup.setBackgroundColor(.clear)
+                })
+            }
+            print("found!")
+        }
+        
+        porcupineManager = PorcupineManager(
+            modelFilePath: modelFilePath!,
+            keywordFilePath: keywordFilePath!,
+            sensitivity: 0.5,
+            keywordCallback: keywordCallback)
+        
+        do {
+            try porcupineManager.start()
+        } catch {
+            print("Error starting manager")
+        }
+        
+    }
+
+    
+    
+    
+    
+    
+    private func initializeAudioSession() {
+        let recordingSession = AVAudioSession.sharedInstance()
+        do {
+            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission { (allowed) in
+                DispatchQueue.main.async {
+                    if allowed {
+                        print("allowed")
+                    } else {
+                        print("disallowed")
+                    }
+                }
+            }
+        } catch {
+            print("error intializing audio session")
+        }
     }
     
 }
