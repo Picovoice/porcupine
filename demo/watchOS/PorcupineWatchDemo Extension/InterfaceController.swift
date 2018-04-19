678d3dd1b906d3cd547b38bf8356d64a4e80e270
Dominique <yahyavi10@gmail.com>
2018-04-19 09:56:23 -0500

    added watch target

---
+++ demo/watchOS/PorcupineWatchDemo Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  PorcupineWatchDemo Extension
+//
+//  Created by Dominique Yahyavi on 4/19/18.
+//  Copyright © 2018 Picovoice. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
