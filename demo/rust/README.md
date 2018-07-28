08f7373bc7a6207ee4a2ffeeba9b302a990d9111
Lilis Iskandar <lilis@veand.co>
2018-07-28 15:38:14 +0000

    Update Rust demo to use default audio input and convert samples to Porcupine's format. (#58)
    fixes to rust demo on macOS

--- demo/rust/README.md
+++ demo/rust/README.md
@@ -1,6 +1,6 @@
 # Porcupine Demo for Rust
 
-This is an example demo written in Rust that uses the [porcupine-sys](https://crates.io/crates/porcupine-sys) crate.
+This is an example demo written in Rust that uses the [porcupine-sys](https://crates.io/crates/porcupine-sys) crate. The demo has been tested to work on Linux and macOS.
 
 # Running the demo
 
@@ -11,7 +11,3 @@ To run this demo, simply enter this in your terminal:
 You can also use multiple keyword files by providing more `-k` options such as:
 
 `cargo run -- -m path/to/porcupine_params.pv -k [path/to/keyword_a.ppn] -k [path/to/keyword_b.ppn] -k [path/to/keyword_c.ppn]`
-
-## Known issues
-
-Unfortunately on macOS, it doesn't seem like it supports opening microphone under the single-channel, 16-bit, and 16KHz sample rate format. In the future, we might need to use the default input format and convert them into Porcupine-compatible format.
--- demo/rust/src/main.rs
+++ demo/rust/src/main.rs
@@ -2,31 +2,49 @@
 #[macro_use]
 extern crate chan;
 extern crate chan_signal;
-use chan_signal::Signal;
 
 // Clap
 extern crate clap;
-use clap::{Arg, App};
+use clap::{App, Arg};
 
 // cpal
 extern crate cpal;
+
+// sample
+extern crate sample;
+use sample::Signal;
 
 // Porcupine
 extern crate porcupine_sys;
 use porcupine_sys as pv;
 
-use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 use std::thread;
 
 fn main() {
     // Setup command-line options
     let matches = App::new("Porcupine Rust Demo")
-            .version("0.1.0")
-            .about("This is a simple Rust program that demonstrates how to use Porcupine")
-            .arg(Arg::with_name("model-file-path").short("m").long("model-file-path").value_name("FILE").help("Sets the model file path").takes_value(true))
-            .arg(Arg::with_name("keyword-file-path").short("k").long("keyword-file-path").value_name("FILE").help("Sets the keyword file path(s)").takes_value(true).multiple(true))
-            .get_matches();
+        .version("0.1.0")
+        .about("This is a simple Rust program that demonstrates how to use Porcupine")
+        .arg(
+            Arg::with_name("model-file-path")
+                .short("m")
+                .long("model-file-path")
+                .value_name("FILE")
+                .help("Sets the model file path")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("keyword-file-path")
+                .short("k")
+                .long("keyword-file-path")
+                .value_name("FILE")
+                .help("Sets the keyword file path(s)")
+                .takes_value(true)
+                .multiple(true),
+        )
+        .get_matches();
     let model_file_path = matches.value_of("model-file-path");
     if model_file_path.is_none() {
         println!("Must provide path to the model file!");
@@ -42,16 +60,22 @@ fn main() {
     let model_file_path = model_file_path.unwrap();
 
     // Set keyword file paths
-    let keyword_file_paths: Vec<String> = keyword_file_paths.unwrap().map(|val| val.to_string()).collect();
+    let keyword_file_paths: Vec<String> = keyword_file_paths
+        .unwrap()
+        .map(|val| val.to_string())
+        .collect();
 
     // Set sensitivities
     let sensitivities: Vec<f32> = keyword_file_paths.iter().map(|_val| 0.5).collect();
 
     // Setup microphone
     let device = cpal::default_input_device().expect("Failed to get default input device");
-    let format = cpal::Format { channels: 1, sample_rate: cpal::SampleRate(16000), data_type: cpal::SampleFormat::I16 };
+    let format = device
+        .default_input_format()
+        .expect("Failed to get default input format");
     let event_loop = Arc::new(cpal::EventLoop::new());
-    let stream_id = event_loop.build_input_stream(&device, &format)
+    let stream_id = event_loop
+        .build_input_stream(&device, &format)
         .expect("Failed to build input stream");
     event_loop.play_stream(stream_id.clone());
 
@@ -60,17 +84,21 @@ fn main() {
     println!("Default input format: {:?}", format);
 
     // Setup OS signals
-    let signal = chan_signal::notify(&[Signal::INT, Signal::TERM]);
+    let signal = chan_signal::notify(&[chan_signal::Signal::INT, chan_signal::Signal::TERM]);
 
     // Setup Porcupine
     let frame_length = unsafe { pv::frame_length() };
-    let object =
-        if keyword_file_paths.len() == 1 {
-            unsafe { pv::Object::new(&model_file_path, &keyword_file_paths[0], sensitivities[0]).unwrap() }
-        } else {
-            let keyword_file_paths: Vec<&str> = keyword_file_paths.iter().map(|s| s.as_str()).collect();
-            unsafe { pv::Object::new_multiple_keywords(&model_file_path, &keyword_file_paths, &sensitivities).unwrap() }
-        };
+    let object = if keyword_file_paths.len() == 1 {
+        unsafe {
+            pv::Object::new(&model_file_path, &keyword_file_paths[0], sensitivities[0]).unwrap()
+        }
+    } else {
+        let keyword_file_paths: Vec<&str> = keyword_file_paths.iter().map(|s| s.as_str()).collect();
+        unsafe {
+            pv::Object::new_multiple_keywords(&model_file_path, &keyword_file_paths, &sensitivities)
+                .unwrap()
+        }
+    };
 
     // Start the microphone and detect keyword
     let recording = Arc::new(AtomicBool::new(true));
@@ -86,12 +114,51 @@ fn main() {
                 return;
             }
 
-            // Otherwise process audio for keyword detection
-            if let cpal::StreamData::Input { buffer: cpal::UnknownTypeInputBuffer::I16(buffer) } = data {
-                let samples: Vec<i16> = buffer.iter().cloned().collect();
+            // Create a closure for detecting keyword
+            let detect_keyword = |samples_buffer: &mut Vec<i16>,
+                                  samples: Vec<i16>,
+                                  sample_rate: u32,
+                                  channels: u16| {
+                // Read the interleaved samples and convert them to a signal.
+                if channels == 2 {
+                    let signal = sample::signal::from_interleaved_samples_iter::<_, [i16; 2]>(
+                        samples.iter().cloned(),
+                    );
 
-                // Add audio data to the buffer
-                samples_buffer.extend_from_slice(&samples);
+                    // Convert the signal's sample rate using `Sinc` interpolation.
+                    let ring_buffer = sample::ring_buffer::Fixed::from([[0i16, 0i16]; 100]);
+                    let sinc = sample::interpolate::Sinc::new(ring_buffer);
+                    let new_signal = sample::interpolate::Converter::from_hz_to_hz(
+                        signal,
+                        sinc,
+                        sample_rate as f64,
+                        16000 as f64,
+                    );
+
+                    // Add audio data to the buffer
+                    let new_samples: Vec<i16> =
+                        new_signal.until_exhausted().map(|frame| frame[0]).collect();
+                    samples_buffer.extend_from_slice(&new_samples);
+                } else {
+                    let signal = sample::signal::from_interleaved_samples_iter::<_, [i16; 1]>(
+                        samples.iter().cloned(),
+                    );
+
+                    // Convert the signal's sample rate using `Sinc` interpolation.
+                    let ring_buffer = sample::ring_buffer::Fixed::from([[0i16]; 100]);
+                    let sinc = sample::interpolate::Sinc::new(ring_buffer);
+                    let new_signal = sample::interpolate::Converter::from_hz_to_hz(
+                        signal,
+                        sinc,
+                        sample_rate as f64,
+                        16000 as f64,
+                    );
+
+                    // Add audio data to the buffer
+                    let new_samples: Vec<i16> =
+                        new_signal.until_exhausted().map(|frame| frame[0]).collect();
+                    samples_buffer.extend_from_slice(&new_samples);
+                }
 
                 // Process buffer when it reaches Porcupine frame length
                 if samples_buffer.len() >= frame_length {
@@ -110,6 +177,55 @@ fn main() {
                         }
                     }
                 }
+            };
+
+            // Otherwise process audio for keyword detection
+            match data {
+                cpal::StreamData::Input {
+                    buffer: cpal::UnknownTypeInputBuffer::I16(buffer),
+                } => {
+                    let samples: Vec<i16> = buffer.iter().cloned().collect();
+
+                    detect_keyword(
+                        &mut samples_buffer,
+                        samples,
+                        format.sample_rate.0,
+                        format.channels,
+                    );
+                }
+                cpal::StreamData::Input {
+                    buffer: cpal::UnknownTypeInputBuffer::F32(buffer),
+                } => {
+                    let samples: Vec<f32> = buffer.iter().cloned().collect();
+                    let samples: Vec<i16> = samples
+                        .iter()
+                        .map(|val| sample::conv::f32::to_i16(*val))
+                        .collect();
+
+                    detect_keyword(
+                        &mut samples_buffer,
+                        samples,
+                        format.sample_rate.0,
+                        format.channels,
+                    );
+                }
+                cpal::StreamData::Input {
+                    buffer: cpal::UnknownTypeInputBuffer::U16(buffer),
+                } => {
+                    let samples: Vec<u16> = buffer.iter().cloned().collect();
+                    let samples: Vec<i16> = samples
+                        .iter()
+                        .map(|val| sample::conv::u16::to_i16(*val))
+                        .collect();
+
+                    detect_keyword(
+                        &mut samples_buffer,
+                        samples,
+                        format.sample_rate.0,
+                        format.channels,
+                    );
+                }
+                _ => {}
             }
         });
     });
