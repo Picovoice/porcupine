e652266dc92f0e6d3f1f94bfd9c18343f5b54d1b
Greg <kiza.soza@gmail.com>
2018-09-20 19:19:57 +1000

    Update compile.sh

--- demo/Alsa cpp/compile.sh
+++ demo/Alsa cpp/compile.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-g++ -o alsademo -I../../include -L../../lib/linux/x86_64 p.cpp -lpv_porcupine -lasound
+
+echo "Building Porcupine Alsa Demo.."
+
+lowercase(){
+    echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
+}
+
+SYSTEM=`lowercase \`uname\``
+MACHINE="`uname -m`"
+
+g++ -o alsademo -I../../include -L../../lib/$SYSTEM/$MACHINE main.cpp -lpv_porcupine -lasound
+export LD_LIBRARY_PATH=../../lib/$SYSTEM/$MACHINE
+
+echo "Build Complete"
+echo "Use ./alsademo to begin"
+echo "read the README.MD for any extra information"
