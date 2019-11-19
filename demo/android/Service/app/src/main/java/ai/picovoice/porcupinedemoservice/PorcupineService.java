a7f519f0623828465aabad4a5a811ce5025d0050
Alireza Kenarsari <alireza@picovoice.ai>
2019-11-18 18:52:44 -0800

    notification works

--- demo/android/Service/app/src/main/java/ai/picovoice/porcupinedemoservice/PorcupineService.java
+++ demo/android/Service/app/src/main/java/ai/picovoice/porcupinedemoservice/PorcupineService.java
@@ -5,12 +5,14 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
 import java.io.File;
@@ -22,6 +24,7 @@ public class PorcupineService extends Service {
     public static final String CHANNEL_ID = "ForegroundServiceChannel";
 
     private PorcupineManager porcupineManager;
+
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -43,7 +46,7 @@ public class PorcupineService extends Service {
                 .setContentIntent(pendingIntent)
                 .build();
 
-        startForeground(1, notification);
+        startForeground(666, notification);
 
         String keywordFilePath = new File(this.getFilesDir(), "porcupine_android.ppn")
                 .getAbsolutePath();
@@ -55,6 +58,19 @@ public class PorcupineService extends Service {
                     keywordFilePath,
                     0.5f,
                     (keywordIndex) -> {
+                        CharSequence title = "Porcupine";
+                        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                                0, new Intent(this, MainActivity.class), 0);
+
+                        Notification notification1 = new NotificationCompat.Builder(this, CHANNEL_ID)
+                                .setContentTitle(title)
+                                .setContentText("detected")
+                                .setSmallIcon(R.drawable.ic_launcher_background)
+                                .setContentIntent(contentIntent).build();
+
+                        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                        mNotificationManager.notify(666, notification1);
+
                         Log.i("", "detected " + keywordIndex);
                     });
             porcupineManager.start();
