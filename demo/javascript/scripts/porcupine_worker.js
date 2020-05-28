3b4feaceff5b5ded91360cb6f5a00bb61be539a5
David Bartle <dbartle@gmail.com>
2020-05-28 11:59:02 -0700

    Format porcupine_worker.js

--- demo/javascript/scripts/porcupine_worker.js
+++ demo/javascript/scripts/porcupine_worker.js
@@ -13,17 +13,17 @@ importScripts("pv_porcupine.js");
 importScripts("porcupine.js");
 
 onmessage = function (e) {
-    switch (e.data.command) {
-        case "init":
-            init(e.data.keywordIDs, e.data.sensitivities);
-            break;
-        case "process":
-            process(e.data.inputFrame);
-            break;
-        case "release":
-            release();
-            break;
-    }
+  switch (e.data.command) {
+    case "init":
+      init(e.data.keywordIDs, e.data.sensitivities);
+      break;
+    case "process":
+      process(e.data.inputFrame);
+      break;
+    case "release":
+      release();
+      break;
+  }
 };
 
 let keywordIDArray;
@@ -33,28 +33,30 @@ let sensitivities;
 let porcupine = null;
 
 function init(keywordIDs, _sensitivities_) {
-    keywordIDArray = Object.values(keywordIDs);
-    keywords = Object.keys(keywordIDs);
-    sensitivities = _sensitivities_;
+  keywordIDArray = Object.values(keywordIDs);
+  keywords = Object.keys(keywordIDs);
+  sensitivities = _sensitivities_;
 
-    if (Porcupine.isLoaded()) {
-        porcupine = Porcupine.create(keywordIDArray, sensitivities);
-    }
+  if (Porcupine.isLoaded()) {
+    porcupine = Porcupine.create(keywordIDArray, sensitivities);
+  }
 }
 
 function process(inputFrame) {
-    if (porcupine == null && Porcupine.isLoaded()) {
-        porcupine = Porcupine.create(keywordIDArray, sensitivities);
-    } else if (porcupine != null) {
-        let keywordIndex = porcupine.process(inputFrame);
-        postMessage({keyword: keywordIndex === -1 ? null : keywords[keywordIndex]});
-    }
+  if (porcupine == null && Porcupine.isLoaded()) {
+    porcupine = Porcupine.create(keywordIDArray, sensitivities);
+  } else if (porcupine != null) {
+    let keywordIndex = porcupine.process(inputFrame);
+    postMessage({
+      keyword: keywordIndex === -1 ? null : keywords[keywordIndex],
+    });
+  }
 }
 
 function release() {
-    if (porcupine !== null) {
-        porcupine.release();
-    }
+  if (porcupine !== null) {
+    porcupine.release();
+  }
 
-    porcupine = null;
+  porcupine = null;
 }
