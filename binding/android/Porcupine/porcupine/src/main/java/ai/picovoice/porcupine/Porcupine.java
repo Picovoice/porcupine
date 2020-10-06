660b74cd44f93aef9885562564649ba613ca520e
Alireza Kenarsari <alireza@picovoice.ai>
2020-10-06 08:09:55 -0700

    refactoring binding

--- binding/android/Porcupine/porcupine/src/main/java/ai/picovoice/porcupine/Porcupine.java
+++ binding/android/Porcupine/porcupine/src/main/java/ai/picovoice/porcupine/Porcupine.java
@@ -1,5 +1,5 @@
 /*
-    Copyright 2018 Picovoice Inc.
+    Copyright 2018-2020 Picovoice Inc.
 
     You may not use this file except in compliance with the license. A copy of the license is
     located in the "LICENSE" file accompanying this source.
@@ -14,28 +14,33 @@ package ai.picovoice.porcupine;
 
 
 /**
- * Android binding for Picovoice's wake word detection engine (Porcupine).
+ * Android binding for Porcupine wake word engine. It detects utterances of given keywords within an
+ * incoming stream of audio in real-time. It processes incoming audio in consecutive frames and for
+ * each frame emits the detection result. The number of samples per frame can be attained by calling
+ * {@link #getFrameLength()}. The incoming audio needs to have a sample rate equal to
+ * {@link #getSampleRate()} and be 16-bit linearly-encoded. Porcupine operates on single-channel
+ * audio.
  */
 public class Porcupine {
     static {
         System.loadLibrary("pv_porcupine");
     }
 
-    private final long object;
+    private final long handle;
 
     /**
      * Constructor.
      *
-     * @param modelFilePath   Absolute path to file containing model parameters.
-     * @param keywordFilePath Absolute path to keyword model file.
-     * @param sensitivity     Sensitivity parameter. A higher sensitivity value lowers miss rate at
-     *                        the cost of increased false alarm rate. A sensitivity value should be
-     *                        within [0, 1].
-     * @throws PorcupineException if there is an error while initializing Porcupine.
+     * @param modelPath   Absolute path to the file containing model parameters.
+     * @param keywordPath Absolute path to keyword model file.
+     * @param sensitivity Sensitivity for detecting keyword. Should be a floating point number
+     *                    within [0, 1]. A higher sensitivity results in fewer misses at the cost of
+     *                    increasing false alarm rate.
+     * @throws PorcupineException If there is an error while initializing Porcupine.
      */
-    public Porcupine(String modelFilePath, String keywordFilePath, float sensitivity) throws PorcupineException {
+    public Porcupine(String modelPath, String keywordPath, float sensitivity) throws PorcupineException {
         try {
-            object = init(modelFilePath, new String[]{keywordFilePath}, new float[]{sensitivity});
+            handle = init(modelPath, new String[]{keywordPath}, new float[]{sensitivity});
         } catch (Exception e) {
             throw new PorcupineException(e);
         }
@@ -44,63 +49,70 @@ public class Porcupine {
     /**
      * Constructor.
      *
-     * @param modelFilePath    Absolute path to file containing model parameters.
-     * @param keywordFilePaths Array of absolute paths to keyword files.
-     * @param sensitivities    Array of sensitivity parameters.
-     * @throws PorcupineException if there is an error while initializing Porcupine.
+     * @param modelPath     Absolute path to the file containing model parameters.
+     * @param keywordPaths  Absolute paths to keyword model files.
+     * @param sensitivities Sensitivities for detecting keywords. Each value should be a number
+     *                      within [0, 1]. A higher sensitivity results in fewer misses at the cost
+     *                      of increasing the false alarm rate.
+     * @throws PorcupineException If there is an error while initializing Porcupine.
      */
-    public Porcupine(
-            String modelFilePath,
-            String[] keywordFilePaths,
-            float[] sensitivities) throws PorcupineException {
+    public Porcupine(String modelPath, String[] keywordPaths, float[] sensitivities) throws PorcupineException {
         try {
-            object = init(modelFilePath, keywordFilePaths, sensitivities);
+            handle = init(modelPath, keywordPaths, sensitivities);
         } catch (Exception e) {
             throw new PorcupineException(e);
         }
     }
 
     /**
-     * Number of audio samples per frame expected by C library.
-     *
-     * @return acceptable number of audio samples per frame.
+     * Releases resources acquired by Porcupine.
      */
-    public native int getFrameLength();
+    public void delete() {
+        delete(handle);
+    }
 
     /**
-     * Audio sample rate accepted by Porcupine.
+     * Processes a frame of the incoming audio stream and emits the detection result.
      *
-     * @return sample rate acceptable by Porcupine.
-     */
-    public native int getSampleRate();
-
-    /**
-     * Monitors incoming audio stream for keywords.
-     *
-     * @param pcm An array of consecutive audio samples. The number of samples per frame can be
-     *            attained by calling {@link #getFrameLength()}. The incoming audio needs to have a
-     *            sample rate equal to {@link #getSampleRate()} and be 16-bit linearly-encoded.
-     *            Porcupine operates on single-channel audio.
-     * @return Index of detected keyword. Indexing is 0-based and according to ordering of keyword
-     * files passed to constructor. When no keyword is detected it returns -1.
+     * @param pcm A frame of audio samples. The number of samples per frame can be attained by
+     *            calling {@link #getFrameLength()}. The incoming audio needs to have a sample rate
+     *            equal to {@link #getSampleRate()} and be 16-bit linearly-encoded. Porcupine
+     *            operates on single-channel audio.
+     * @return Index of observed keyword at the end of the current frame. Indexing is
+     * 0-based and matches the ordering of keyword models provided to the constructor. If no keyword
+     * is detected then it returns -1.
      * @throws PorcupineException if there is an error while processing the audio sample.
      */
     public int process(short[] pcm) throws PorcupineException {
         try {
-            return process(object, pcm);
+            return process(handle, pcm);
         } catch (Exception e) {
             throw new PorcupineException(e);
         }
     }
 
     /**
-     * Releases resources acquired.
+     * Getter for version.
+     *
+     * @return Version.
      */
-    public void delete() {
-        delete(object);
-    }
+    public native String getVersion();
 
-    private native long init(String modelFilePath, String[] keywordFilePaths, float[] sensitivities);
+    /**
+     * Getter for number of audio samples per frame..
+     *
+     * @return Number of audio samples per frame.
+     */
+    public native int getFrameLength();
+
+    /**
+     * Getter for audio sample rate accepted by Picovoice.
+     *
+     * @return Audio sample rate accepted by Picovoice.
+     */
+    public native int getSampleRate();
+
+    private native long init(String modelPath, String[] keywordPaths, float[] sensitivities);
 
     private native void delete(long object);
 
