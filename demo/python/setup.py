a9cf8582576a1a4b51b3a203dbf9807e4b1deaec
Pouya Rostam <pouya@picovoice.ai>
2021-03-05 11:00:54 -0800

    update demo setup info

--- demo/python/setup.py
+++ demo/python/setup.py
@@ -28,7 +28,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
 
 setuptools.setup(
     name="pvporcupinedemo",
-    version="1.9.3",
+    version="1.9.4",
     author="Picovoice",
     author_email="hello@picovoice.ai",
     description="Porcupine wake word engine demos",
@@ -36,7 +36,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Picovoice/porcupine",
     packages=["pvporcupinedemo"],
-    install_requires=["enum34", "numpy", "pvporcupine==1.9.3", "pyaudio", "soundfile"],
+    install_requires=["enum34", "numpy", "pvporcupine==1.9.4", "pyaudio", "soundfile"],
     include_package_data=True,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
