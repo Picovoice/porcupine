3a96d1b58b2de97be73071f8c3b2beae8177fd99
David Bartle <dbartle@picovoice.ai>
2021-03-23 14:56:15 -0700

    React hook demo

---
+++ demo/react/README.md
@@ -0,0 +1,21 @@
+# porcupine-web-react-demo
+
+This demo application includes the `VoiceWidget` which uses the `usePorcupine` react hook to allow listening for keywords. The keywords are handled via the `keywordEventHandler` callback function that updates a React `useState` hook, and then renders the results.
+
+If you decline microphone permission in the browser, or another such issue prevents Porcupine from starting, the error will be displayed.
+
+The widget shows the various loading and error states, as well as mounting/unmounting the `VoiceWidget` with a toggle, demonstrating the complete lifecycle of Porcupine with in a React app.
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Install and Run
+
+```bash
+yarn
+yarn start
+```
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
---
+++ demo/react/src/VoiceWidget.jsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import { usePorcupine } from "@picovoice/porcupine-web-react";
+
+export default function VoiceWidget() {
+  const [keywordDetections, setKeywordDetections] = useState([]);
+  const [workerChunk, setWorkerChunk] = useState({ factory: null });
+  const [porcupineFactoryArgs] = useState([
+    { builtin: "Alexa", sensitivity: 0.7 },
+    "Picovoice",
+  ]);
+
+  useEffect(() => {
+    async function loadPorcupine() {
+      // Dynamically import the worker
+      const ppnEnFactory = (await import("@picovoice/porcupine-web-en-worker"))
+        .PorcupineWorkerFactory;
+      console.log("Porcupine worker (EN) chunk is loaded.");
+      return ppnEnFactory;
+    }
+
+    if (workerChunk.factory === null) {
+      loadPorcupine().then((workerFactory) => {
+        setWorkerChunk({ factory: workerFactory });
+      });
+    }
+  }, [workerChunk]);
+
+  const keywordEventHandler = (porcupineKeywordLabel) => {
+    setKeywordDetections((x) => [...x, porcupineKeywordLabel]);
+  };
+
+  const {
+    isLoaded,
+    isListening,
+    isError,
+    errorMessage,
+    start,
+    resume,
+    pause,
+  } = usePorcupine(
+    workerChunk.factory,
+    { porcupineFactoryArgs: porcupineFactoryArgs, start: true },
+    keywordEventHandler
+  );
+
+  return (
+    <div className="voice-widget">
+      <h2>VoiceWidget</h2>
+      <h3>Loaded: {JSON.stringify(isLoaded)}</h3>
+      <h3>Listening: {JSON.stringify(isListening)}</h3>
+      <h3>Error: {JSON.stringify(isError)}</h3>
+      {isError && (
+        <p className="error-message">{JSON.stringify(errorMessage)}</p>
+      )}
+      <h3>Keywords: {JSON.stringify(porcupineFactoryArgs)}</h3>
+      <br />
+      <button
+        onClick={() => start()}
+        disabled={isError || isListening || !isLoaded}
+      >
+        Start
+      </button>
+      <button
+        onClick={() => pause()}
+        disabled={isError || !isListening || !isLoaded}
+      >
+        Pause
+      </button>
+      <button
+        onClick={() => resume()}
+        disabled={isError || isListening || !isLoaded}
+      >
+        Resume
+      </button>
+      <h3>Keyword Detections:</h3>
+      {keywordDetections.length > 0 && (
+        <ul>
+          {keywordDetections.map((label, index) => (
+            <li key={index}>{label}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
