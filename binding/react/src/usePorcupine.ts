c9c5d0c9ba3c192aa002d7835b01800ab59d7f67
David Bartle <dbartle@picovoice.ai>
2021-03-23 13:11:35 -0700

    React: Upgrade WebVoiceProcessor to 2.0 / change to named import

--- binding/react/src/usePorcupine.ts
+++ binding/react/src/usePorcupine.ts
@@ -11,7 +11,7 @@
 
 import { useState, useEffect, useRef } from 'react';
 
-import WebVoiceProcessor from '@picovoice/web-voice-processor';
+import { WebVoiceProcessor } from '@picovoice/web-voice-processor';
 
 import {
   PorcupineHookArgs,
