dd9a8f3bb8dd332c045b431d857eeb76cf801411
Ian Lavery <ian@picovoice.ai>
2021-05-19 12:24:55 -0700

    porcupine init call change

--- demo/go/filedemo/porcupine_file_demo.go
+++ demo/go/filedemo/porcupine_file_demo.go
@@ -1,3 +1,13 @@
+// Copyright 2021 Picovoice Inc.
+//
+// You may not use this file except in compliance with the license. A copy of the license is
+// located in the "LICENSE" file accompanying this source.
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the
+// License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -97,9 +107,9 @@ func main() {
 		}
 	}
 
-	status, err := p.Init()
-	if status != 0 || err != nil {
-		log.Fatal(status, err)
+	err = p.Init()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	buf := &audio.IntBuffer{
