c01fd9291e5aa88bded6a759ddaadd6486058db3
Ian Lavery <ian@picovoice.ai>
2021-07-07 14:18:51 -0700

    all tests in place for ios

--- demo/ios/PorcupineDemoUITests/PorcupineDemoUITests.swift
+++ demo/ios/PorcupineDemoUITests/PorcupineDemoUITests.swift
@@ -1,24 +1,177 @@
-import XCTest
 import AVFoundation
+import XCTest
+
 import Porcupine
 
 class PorcupineDemoUITests: XCTestCase {
 
-    let app = XCUIApplication()
-
     override func setUp() {
         super.setUp()
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        app.launch()
     }
 
     override func tearDown() {
-       // Put teardown code here. This method is called after the invocation of each test method in the class.
        super.tearDown()
    }
 
     func testInitSuccessWithSingleBuiltIn() throws {
+        let p = try Porcupine.init(keyword: Porcupine.BuiltInKeyword.porcupine)
+        XCTAssert(Porcupine.version != "")
+        XCTAssert(Porcupine.frameLength > 0)
+        XCTAssert(Porcupine.sampleRate > 0)
+        p.delete()
+    }
+    
+    func testInitSuccessWithMultipleBuiltIns() throws {
+        let keywords = [
+            Porcupine.BuiltInKeyword.alexa,
+            Porcupine.BuiltInKeyword.grasshopper,
+            Porcupine.BuiltInKeyword.picovoice,
+            Porcupine.BuiltInKeyword.terminator
+        ]
+        
+        let p = try Porcupine.init(keywords: keywords)
+        p.delete()
+    }
+    
+    func testInitSuccessWithCustomArguments() throws {
+        let bundle = Bundle(for: type(of: self))
+        let keywordPath = bundle.path(forResource: "hey barista_ios", ofType: "ppn")!
+        let modelPath = bundle.path(forResource: "porcupine_params", ofType: "pv")
+        
+        let p = try Porcupine.init(keywordPath: keywordPath, modelPath: modelPath)
+        p.delete()
+    }
+    
+    func testInitSuccessWithMultipleCustomKeywordsAndSensitivities() throws {
+        let bundle = Bundle(for: type(of: self))
+        let keywordPath = bundle.path(forResource: "hey barista_ios", ofType: "ppn")!
+        let keywordPath2 = bundle.path(forResource: "pico clock_ios", ofType: "ppn")!
+        
+        let p = try Porcupine.init(keywordPaths: [keywordPath, keywordPath2], sensitivities: [0.35, 0.6])
+        p.delete()
+    }
+    
+    func testInitFailWithMismatchedLanguage() throws {
+        let bundle = Bundle(for: type(of: self))
+        let keywordPath = bundle.path(forResource: "ananas_ios", ofType: "ppn")!
+        let modelPath = bundle.path(forResource: "porcupine_params", ofType: "pv")!
+        
+        var didFail = false
+        do {
+            _ = try Porcupine.init(keywordPath: keywordPath, modelPath: modelPath)
+        } catch {
+            didFail = true
+        }
+        
+        assert(didFail)
+    }
+    
+    func testInitFailWithInvalidModelPath() throws {
+        
+        var didFail = false
+        do {
+            _ = try Porcupine.init(keyword: Porcupine.BuiltInKeyword.porcupine, modelPath: "bad_path/bad_path.pv")
+        } catch {
+            didFail = true
+        }
+        
+        assert(didFail)
+    }
+    
+    func testInitFailWithInvalidSensitivity() throws {
+        
+        var didFail = false
+        do {
+            _ = try Porcupine.init(keyword: Porcupine.BuiltInKeyword.porcupine, sensitivity: 10)
+        } catch {
+            didFail = true
+        }
+        
+        assert(didFail)
+    }
+    
+    func testInitFailWithMismatchedSensitivityCount() throws {
+        
+        let keywords = [Porcupine.BuiltInKeyword.porcupine, Porcupine.BuiltInKeyword.grasshopper]
+        let sensitivities:[Float32] = [0.3, 0.8, 0.4]
+        var didFail = false
+        do {
+            _ = try Porcupine.init(keywords: keywords, sensitivities: sensitivities)
+        } catch {
+            didFail = true
+        }
+        
+        assert(didFail)
+    }
+    
+    func testInitFailWithNoKeywords() throws {
+        
+        let keywords:[Porcupine.BuiltInKeyword] = []
+        var didFail = false
+        do {
+            _ = try Porcupine.init(keywords: keywords)
+        } catch {
+            didFail = true
+        }
+        
+        assert(didFail)
+    }
+    
+    func testInitFailWithInvalidKeywordPath() throws {
+        
+        var didFail = false
+        do {
+            _ = try Porcupine.init(keywordPath: "bad_path/bad_path.ppn")
+        } catch {
+            didFail = true
+        }
+        
+        assert(didFail)
+    }
+    
+    func testInitFailWithWrongKeywordPlatform() throws {
+        let bundle = Bundle(for: type(of: self))
+        let keywordPath = bundle.path(forResource: "alexa_linux", ofType: "ppn")!
+        
+        var didFail = false
+        do {
+            _ = try Porcupine.init(keywordPath: keywordPath)
+        } catch {
+            didFail = true
+        }
+        
+        assert(didFail)
+    }
+    
+    func testInitSuccessES() throws {
+        let bundle = Bundle(for: type(of: self))
+        let keywordPath = bundle.path(forResource: "emparedado_ios", ofType: "ppn")!
+        let modelPath = bundle.path(forResource: "porcupine_params_es", ofType: "pv")!
+        
+        let p = try Porcupine.init(keywordPath: keywordPath, modelPath: modelPath)
+        p.delete()
+    }
+    
+    func testInitSuccessFR() throws {
+        let bundle = Bundle(for: type(of: self))
+        let keywordPath = bundle.path(forResource: "framboise_ios", ofType: "ppn")!
+        let modelPath = bundle.path(forResource: "porcupine_params_fr", ofType: "pv")!
+        
+        let p = try Porcupine.init(keywordPath: keywordPath, modelPath: modelPath)
+        p.delete()
+    }
+    
+    func testInitSuccessDE() throws {
+        let bundle = Bundle(for: type(of: self))
+        let keywordPath = bundle.path(forResource: "ananas_ios", ofType: "ppn")!
+        let modelPath = bundle.path(forResource: "porcupine_params_de", ofType: "pv")!
+        
+        let p = try Porcupine.init(keywordPath: keywordPath, modelPath: modelPath)
+        p.delete()
+    }
+    
+    func testProcSuccessWithSingleBuiltIn() throws {
         let p:Porcupine = try Porcupine.init(keyword: Porcupine.BuiltInKeyword.porcupine)
         
         let bundle = Bundle(for: type(of: self))
@@ -43,7 +196,7 @@ class PorcupineDemoUITests: XCTestCase {
         XCTAssert(results.count == 1 && results[0] == 0)
     }
     
-    func testInitSuccessWithMultipleBuiltIns() throws {
+    func testProcSuccessWithMultipleBuiltIns() throws {
         let keywords = [
             Porcupine.BuiltInKeyword.alexa,
             Porcupine.BuiltInKeyword.americano,
