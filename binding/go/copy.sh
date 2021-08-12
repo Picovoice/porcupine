363afd6eaae8e746eb404de24d05d5a445ec9b4b
Kwangsoo Yeo <kyeo@picovoice.ai>
2021-08-12 13:54:29 -0700

    Support go bindings for rpi, jetson and beaglebone (#470)

--- binding/go/copy.sh
+++ binding/go/copy.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+KEYWORDS=(alexa americano blueberry bumblebee computer grapefruit grasshopper "hey google"
+ "hey siri" jarvis "ok google" picovoice porcupine terminator)
+
 echo "Copying Porcupine model..."
 cp ../../lib/common/porcupine_params.pv ./embedded/lib/common/porcupine_params.pv
 
@@ -13,10 +18,20 @@ cp ../../lib/windows/amd64/libpv_porcupine.dll ./embedded/lib/windows/amd64/libp
 echo "Copying RPi libs..."
 cp -rp ../../lib/raspberry-pi/* ./embedded/lib/raspberry-pi
 
+echo "Copying Jetson lib..."
+cp ../../lib/jetson/cortex-a57-aarch64/libpv_porcupine.so ./embedded/lib/jetson/cortex-a57-aarch64/libpv_porcupine.so
+
+echo "Copying BeagleBone lib..."
+cp ../../lib/beaglebone/libpv_porcupine.so ./embedded/lib/beaglebone/libpv_porcupine.so
+
 echo "Copying keyword files"
-cp -rp ../../resources/keyword_files/windows/* ./embedded/resources/keyword_files/windows
-cp -rp ../../resources/keyword_files/mac/* ./embedded/resources/keyword_files/mac
-cp -rp ../../resources/keyword_files/linux/* ./embedded/resources/keyword_files/linux
-cp -rp ../../resources/keyword_files/raspberry-pi/* ./embedded/resources/keyword_files/raspberry-pi
+for keyword in "${KEYWORDS[@]}"; do
+    cp ../../resources/keyword_files/windows/"$keyword"_windows.ppn ./embedded/resources/keyword_files/windows/
+    cp ../../resources/keyword_files/mac/"$keyword"_mac.ppn ./embedded/resources/keyword_files/mac/
+    cp ../../resources/keyword_files/linux/"$keyword"_linux.ppn ./embedded/resources/keyword_files/linux/
+    cp ../../resources/keyword_files/raspberry-pi/"$keyword"_raspberry-pi.ppn ./embedded/resources/keyword_files/raspberry-pi/
+    cp ../../resources/keyword_files/jetson/"$keyword"_jetson.ppn ./embedded/resources/keyword_files/jetson/
+    cp ../../resources/keyword_files/beaglebone/"$keyword"_beaglebone.ppn ./embedded/resources/keyword_files/beaglebone/
+done
 
 echo "Copy complete!"
