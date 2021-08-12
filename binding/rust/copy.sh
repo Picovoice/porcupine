8c57f7f234e791305a33e94b150c9926d0c32d65
Eric Mikulin <eric@picovoice.ai>
2021-08-12 16:20:24 -0700

    Test trip Rust github action

--- binding/rust/copy.sh
+++ binding/rust/copy.sh
@@ -2,6 +2,8 @@ echo "Preparing dir ..."
 mkdir -p ./data/lib/
 mkdir -p ./data/resources/
 
+echo! "Test to trip github actions"
+
 echo "Copying Library Files ..."
 cp -r ../../lib/beaglebone ./data/lib/
 cp -r ../../lib/common ./data/lib/
