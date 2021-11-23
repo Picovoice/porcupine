c208d0fb0398f5f9cb015ca467a080bbdf8999a2
Kwangsoo Yeo <kyeo@picovoice.ai>
2021-11-22 16:51:39 -0800

    change errors in iOS to be class like

---
+++ binding/ios/PorcupineErrors.swift
@@ -0,0 +1,42 @@
+//
+//  Copyright 2021 Picovoice Inc.
+//  You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
+//  file accompanying this source.
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+//  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+//  specific language governing permissions and limitations under the License.
+//
+
+class PorcupineError : LocalizedError {
+    private let message: String;
+    
+    init (_ message: String) {
+        self.message = message
+    }
+    
+    public var errorDescription: String? {
+        return message
+    }
+}
+
+class PorcupineMemoryError : PorcupineError {}
+
+class PorcupineIOError : PorcupineError {}
+
+class PorcupineInvalidArgumentError : PorcupineError {}
+
+class PorcupineStopIterationError : PorcupineError {}
+
+class PorcupineKeyError : PorcupineError {}
+
+class PorcupineInvalidStateError : PorcupineError {}
+
+class PorcupineRuntimeError : PorcupineError {}
+
+class PorcupineActivationError : PorcupineError {}
+
+class PorcupineActivationLimitError : PorcupineError {}
+
+class PorcupineActivationThrottledError : PorcupineError {}
+
+class PorcupineActivationRefusedError : PorcupineError {}
