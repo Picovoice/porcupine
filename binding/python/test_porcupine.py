783d95ba92d0f337b6a4ab1d5f147eed8bc6203c
Iliad <ili1dr1m@gmail.com>
2021-12-15 16:39:43 -0800

    change back en audio file name

--- binding/python/test_porcupine.py
+++ binding/python/test_porcupine.py
@@ -57,7 +57,7 @@ class PorcupineTestCase(unittest.TestCase):
             ground_truth = [0])        
 
         self.run_porcupine(lang = lang,
-            audio_file_name = 'multiple_keywords_en.wav',
+            audio_file_name = 'multiple_keywords.wav',
             keywords = ['americano', 'blueberry', 'bumblebee', 'grapefruit', 'grasshopper', 'picovoice', 'porcupine', 'terminator'],
             ground_truth = [6, 0, 1, 2, 3, 4, 5, 6, 7])
 
