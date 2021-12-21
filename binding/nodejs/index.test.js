0b20c61355cc95e207da5d6c2c3333346a545802
Iliad <ili1dr1m@gmail.com>
2021-12-20 17:15:50 -0800

    add non-en tests

--- binding/nodejs/index.test.js
+++ binding/nodejs/index.test.js
@@ -18,14 +18,33 @@ const WaveFile = require("wavefile").WaveFile;
 const { PvArgumentError, PvStateError } = require("./errors");
 const { getPlatform } = require("./platforms");
 const { GRASSHOPPER } = require("./builtin_keywords.js");
+const { count } = require("console");
 
 const MODEL_PATH = "./lib/common/porcupine_params.pv";
+const MODEL_PATH_DE = "../../lib/common/porcupine_params_de.pv";
+const MODEL_PATH_ES = "../../lib/common/porcupine_params_es.pv";
+const MODEL_PATH_FR = "../../lib/common/porcupine_params_fr.pv";
 
 const SENSITIVITIES_1 = [0.5];
 const SENSITIVITIES_2 = [0.5, 0.5];
-const WAV_PATH_PORCUPINE = "../../resources/audio_samples/porcupine.wav";
+const SENSITIVITIES_3 = [0.5, 0.5, 0.5];
+const SENSITIVITIES_4 = [0.5, 0.5, 0.5, 0.5];
+const WAV_PATH_PORCUPINE =
+  "../../resources/audio_samples/porcupine.wav";
 const WAV_PATH_MULTIPLE_KEYWORDS =
   "../../resources/audio_samples/multiple_keywords.wav";
+const WAV_PATH_SINGLE_KEYWORD_DE =
+  "../../resources/audio_samples/heuschrecke.wav";
+const WAV_PATH_MULTIPLE_KEYWORDS_DE =
+  "../../resources/audio_samples/multiple_keywords_de.wav";
+const WAV_PATH_SINGLE_KEYWORD_ES =
+  "../../resources/audio_samples/manzana.wav";
+const WAV_PATH_MULTIPLE_KEYWORDS_ES =
+  "../../resources/audio_samples/multiple_keywords_es.wav";
+const WAV_PATH_SINGLE_KEYWORD_FR =
+  "../../resources/audio_samples/mon_chouchou.wav";
+const WAV_PATH_MULTIPLE_KEYWORDS_FR =
+  "../../resources/audio_samples/multiple_keywords_fr.wav";  
 
 const platform = getPlatform();
 
@@ -36,6 +55,32 @@ const keywordPathsMultipleAmericanoBumblebee = [
   `../../resources/keyword_files/${platform}/americano_${platform}.ppn`,
   `../../resources/keyword_files/${platform}/bumblebee_${platform}.ppn`,
 ];
+const keywordPathsSingleDe = [
+  `../../resources/keyword_files_de/${platform}/heuschrecke_${platform}.ppn`,
+];
+const keywordPathsMultipleDe = [
+  `../../resources/keyword_files_de/${platform}/ananas_${platform}.ppn`,
+  `../../resources/keyword_files_de/${platform}/heuschrecke_${platform}.ppn`,
+  `../../resources/keyword_files_de/${platform}/leguan_${platform}.ppn`,
+  `../../resources/keyword_files_de/${platform}/stachelschwein_${platform}.ppn`,  
+];
+const keywordPathsSingleEs = [
+  `../../resources/keyword_files_es/${platform}/manzana_${platform}.ppn`,
+];
+const keywordPathsMultipleEs = [
+  `../../resources/keyword_files_es/${platform}/emparedado_${platform}.ppn`,
+  `../../resources/keyword_files_es/${platform}/leopardo_${platform}.ppn`,
+  `../../resources/keyword_files_es/${platform}/manzana_${platform}.ppn`,
+];
+const keywordPathsSingleFr = [
+  `../../resources/keyword_files_fr/${platform}/mon chouchou_${platform}.ppn`,
+];
+const keywordPathsMultipleFr = [
+  `../../resources/keyword_files_fr/${platform}/framboise_${platform}.ppn`,
+  `../../resources/keyword_files_fr/${platform}/mon chouchou_${platform}.ppn`,
+  `../../resources/keyword_files_fr/${platform}/parapluie_${platform}.ppn`,
+];
+
 
 const ACCESS_KEY = process.argv.filter((x) => x.startsWith('--access_key='))[0].split('--access_key=')[1];
 
@@ -156,6 +201,122 @@ describe("manual paths", () => {
     let porcupineCount = counts.get(0);
     expect(porcupineCount).toEqual(1);
   });
+});
+
+describe("keyword detection in DE", () => {
+  test("single keyword single detection", () => {
+    let porcupineEngine = new Porcupine(
+      ACCESS_KEY,  
+      keywordPathsSingleDe,
+      SENSITIVITIES_1,
+      MODEL_PATH_DE
+    );
+
+    let counts = porcupineDetectionCounts(
+      porcupineEngine,
+      WAV_PATH_SINGLE_KEYWORD_DE
+    );
+    let heuschreckeCount = counts.get(0);
+    expect(heuschreckeCount).toEqual(1);
+  });
+
+  test("multiple keyword multiple detection", () => {
+    let porcupineEngine = new Porcupine(
+      ACCESS_KEY,
+      keywordPathsMultipleDe,
+      SENSITIVITIES_4,
+      MODEL_PATH_DE
+    );
+
+    let counts = porcupineDetectionCounts(
+      porcupineEngine,
+      WAV_PATH_MULTIPLE_KEYWORDS_DE
+    );
+    let ananasCount = counts.get(0);
+    let heuschreckeCount = counts.get(1);
+    let leguanCount = counts.get(2);
+    let stachelschweinCount = counts.get(3);
+    expect(ananasCount).toEqual(1);
+    expect(heuschreckeCount).toEqual(1);
+    expect(leguanCount).toEqual(1);
+    expect(stachelschweinCount).toEqual(1);
+  });  
+});
+
+describe("keyword detection in ES", () => {
+  test("single keyword single detection", () => {
+    let porcupineEngine = new Porcupine(
+      ACCESS_KEY,  
+      keywordPathsSingleEs,
+      SENSITIVITIES_1,
+      MODEL_PATH_ES
+    );
+
+    let counts = porcupineDetectionCounts(
+      porcupineEngine,
+      WAV_PATH_SINGLE_KEYWORD_ES
+    );
+    let manzanaCount = counts.get(0);
+    expect(manzanaCount).toEqual(1);
+  });
+
+  test("multiple keyword multiple detection", () => {
+    let porcupineEngine = new Porcupine(
+      ACCESS_KEY,
+      keywordPathsMultipleEs,
+      SENSITIVITIES_3,
+      MODEL_PATH_ES
+    );
+
+    let counts = porcupineDetectionCounts(
+      porcupineEngine,
+      WAV_PATH_MULTIPLE_KEYWORDS_ES
+    );
+    let emparedadoCount = counts.get(0);
+    let leopardoCount = counts.get(1);
+    let manzanaCount = counts.get(2);
+    expect(emparedadoCount).toEqual(1);
+    expect(leopardoCount).toEqual(1);
+    expect(manzanaCount).toEqual(1);
+  });  
+});
+
+describe("keyword detection in FR", () => {
+  test("single keyword single detection", () => {
+    let porcupineEngine = new Porcupine(
+      ACCESS_KEY,  
+      keywordPathsSingleFr,
+      SENSITIVITIES_1,
+      MODEL_PATH_FR
+    );
+
+    let counts = porcupineDetectionCounts(
+      porcupineEngine,
+      WAV_PATH_SINGLE_KEYWORD_FR
+    );
+    let monChouchouCount = counts.get(0);
+    expect(monChouchouCount).toEqual(1);
+  });
+
+  test("multiple keyword multiple detection", () => {
+    let porcupineEngine = new Porcupine(
+      ACCESS_KEY,
+      keywordPathsMultipleFr,
+      SENSITIVITIES_3,
+      MODEL_PATH_FR
+    );
+
+    let counts = porcupineDetectionCounts(
+      porcupineEngine,
+      WAV_PATH_MULTIPLE_KEYWORDS_FR
+    );
+    let framboiseCount = counts.get(0);
+    let monChouchouCount = counts.get(1);
+    let parapluieCount = counts.get(2);
+    expect(framboiseCount).toEqual(2);
+    expect(monChouchouCount).toEqual(1);
+    expect(parapluieCount).toEqual(1);
+  });  
 });
 
 describe("basic parameter validation", () => {
