967d3432aa05849a2d1e13ab0864558c5385bbeb
Kwangsoo Yeo <kyeo@picovoice.ai>
2022-01-19 15:38:32 -0800

    v2.1 python release

--- binding/python/setup.py
+++ binding/python/setup.py
@@ -67,7 +67,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
 
 setuptools.setup(
     name="pvporcupine",
-    version="2.0.4",
+    version="2.1.0",
     author="Picovoice",
     author_email="hello@picovoice.ai",
     description="Porcupine wake word engine.",
