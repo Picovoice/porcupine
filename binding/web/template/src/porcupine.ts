750c86c11508ea207e90fc11c7232e6a87d3ea33
Kwangsoo Yeo <kyeo@picovoice.ai>
2022-01-28 12:11:58 -0800

    updated web binding to use new web util

--- binding/web/template/src/porcupine.ts
+++ binding/web/template/src/porcupine.ts
@@ -12,29 +12,24 @@
 /* eslint camelcase: 0 */
 
 // @ts-ignore
-import * as Asyncify from 'asyncify-wasm';
 import { Mutex } from 'async-mutex';
 
 import { PorcupineKeyword, PorcupineEngine } from '@picovoice/porcupine-web-core';
 
+import { 
+  aligned_alloc_type, 
+  buildWasm,
+  arrayBufferToStringAtIndex,
+  isAccessKeyValid
+} from '@picovoice/web-utils';
+
 // @ts-ignore
 import { PORCUPINE_WASM_BASE64 } from './lang/porcupine_b64';
-import { wasiSnapshotPreview1Emulator } from './wasi_snapshot';
 
 import {
   BuiltInKeyword,
   BUILT_IN_KEYWORD_BYTES,
 } from './lang/built_in_keywords';
-
-import {
-  arrayBufferToBase64AtIndex,
-  arrayBufferToStringAtIndex,
-  base64ToUint8Array,
-  fetchWithTimeout,
-  getPvStorage,
-  isAccessKeyValid,
-  stringHeaderToObject,
-} from './utils';
 
 const DEFAULT_SENSITIVITY = 0.5;
 const PV_STATUS_SUCCESS = 10000;
@@ -43,7 +38,6 @@ const PV_STATUS_SUCCESS = 10000;
  * WebAssembly function types
  */
 
- type aligned_alloc_type = (alignment: number, size: number) => Promise<number>;
  type pv_porcupine_init_type = (
    accessKey: number, 
    numKeywords: number, 
@@ -340,303 +334,16 @@ export class Porcupine implements PorcupineEngine {
     const memoryBufferInt32 = new Int32Array(memory.buffer);
     const memoryBufferFloat32 = new Float32Array(memory.buffer);
 
-    const storage = getPvStorage();
+    const exports = await buildWasm(memory, PORCUPINE_WASM_BASE64);
 
-    const pvConsoleLogWasm = function (index: number): void {
-      // eslint-disable-next-line no-console
-      console.log(arrayBufferToStringAtIndex(memoryBufferUint8, index));
-    };
-
-    const pvAssertWasm = function (
-      expr: number,
-      line: number,
-      fileNameAddress: number
-    ): void {
-      if (expr === 0) {
-        const fileName = arrayBufferToStringAtIndex(
-          memoryBufferUint8,
-          fileNameAddress
-        );
-        throw new Error(`assertion failed at line ${line} in "${fileName}"`);
-      }
-    };
-
-    const pvTimeWasm = function (): number {
-      return Date.now() / 1000;
-    };
-
-    const pvHttpsRequestWasm = async function (
-      httpMethodAddress: number,
-      serverNameAddress: number,
-      endpointAddress: number,
-      headerAddress: number,
-      bodyAddress: number,
-      timeoutMs: number,
-      responseAddressAddress: number,
-      responseSizeAddress: number,
-      responseCodeAddress: number
-    ): Promise<void> {
-      const httpMethod = arrayBufferToStringAtIndex(
-        memoryBufferUint8,
-        httpMethodAddress
-      );
-      const serverName = arrayBufferToStringAtIndex(
-        memoryBufferUint8,
-        serverNameAddress
-      );
-      const endpoint = arrayBufferToStringAtIndex(
-        memoryBufferUint8,
-        endpointAddress
-      );
-      const header = arrayBufferToStringAtIndex(
-        memoryBufferUint8,
-        headerAddress
-      );
-      const body = arrayBufferToStringAtIndex(memoryBufferUint8, bodyAddress);
-
-      const headerObject = stringHeaderToObject(header);
-
-      let response: Response;
-      let responseText: string;
-      let statusCode: number;
-
-      try {
-        response = await fetchWithTimeout(
-          'https://' + serverName + endpoint,
-          {
-            method: httpMethod,
-            headers: headerObject,
-            body: body,
-          },
-          timeoutMs
-        );
-        statusCode = response.status;
-      } catch (error) {
-        statusCode = 0;
-      }
-      // @ts-ignore
-      if (response !== undefined) {
-        try {
-          responseText = await response.text();
-        } catch (error) {
-          responseText = '';
-          statusCode = 1;
-        }
-        // eslint-disable-next-line
-        const responseAddress = await aligned_alloc(
-          Int8Array.BYTES_PER_ELEMENT,
-          (responseText.length + 1) * Int8Array.BYTES_PER_ELEMENT
-        );
-        if (responseAddress === 0) {
-          throw new Error('malloc failed: Cannot allocate memory');
-        }
-
-        memoryBufferInt32[
-          responseSizeAddress / Int32Array.BYTES_PER_ELEMENT
-        ] = responseText.length + 1;
-        memoryBufferInt32[
-          responseAddressAddress / Int32Array.BYTES_PER_ELEMENT
-        ] = responseAddress;
-
-        for (let i = 0; i < responseText.length; i++) {
-          memoryBufferUint8[responseAddress + i] = responseText.charCodeAt(i);
-        }
-        memoryBufferUint8[responseAddress + responseText.length] = 0;
-      }
-
-      memoryBufferInt32[
-        responseCodeAddress / Int32Array.BYTES_PER_ELEMENT
-      ] = statusCode;
-    };
-
-    const pvFileLoadWasm = async function (
-      pathAddress: number,
-      numContentBytesAddress: number,
-      contentAddressAddress: number,
-      succeededAddress: number
-    ): Promise<void> {
-      const path = arrayBufferToStringAtIndex(memoryBufferUint8, pathAddress);
-      try {
-        const contentBase64 = await storage.getItem(path);
-        const contentBuffer = base64ToUint8Array(contentBase64);
-        // eslint-disable-next-line
-        const contentAddress = await aligned_alloc(
-          Uint8Array.BYTES_PER_ELEMENT,
-          contentBuffer.length * Uint8Array.BYTES_PER_ELEMENT
-        );
-
-        if (contentAddress === 0) {
-          throw new Error('malloc failed: Cannot allocate memory');
-        }
-
-        memoryBufferInt32[
-          numContentBytesAddress / Int32Array.BYTES_PER_ELEMENT
-        ] = contentBuffer.byteLength;
-        memoryBufferInt32[
-          contentAddressAddress / Int32Array.BYTES_PER_ELEMENT
-        ] = contentAddress;
-        memoryBufferUint8.set(contentBuffer, contentAddress);
-        memoryBufferInt32[
-          succeededAddress / Int32Array.BYTES_PER_ELEMENT
-        ] = 1;
-      } catch (error) {
-        memoryBufferInt32[
-          succeededAddress / Int32Array.BYTES_PER_ELEMENT
-        ] = 0;
-      }
-    };
-
-    const pvFileSaveWasm = async function (
-      pathAddress: number,
-      numContentBytes: number,
-      contentAddress: number,
-      succeededAddress: number
-    ): Promise<void> {
-      const path = arrayBufferToStringAtIndex(memoryBufferUint8, pathAddress);
-      const content = arrayBufferToBase64AtIndex(
-        memoryBufferUint8,
-        numContentBytes,
-        contentAddress
-      );
-      try {
-        await storage.setItem(path, content);
-        memoryBufferInt32[
-          succeededAddress / Int32Array.BYTES_PER_ELEMENT
-        ] = 1;
-      } catch (error) {
-        memoryBufferInt32[
-          succeededAddress / Int32Array.BYTES_PER_ELEMENT
-        ] = 0;
-      }
-    };
-
-    const pvFileExistsWasm = async function (
-      pathAddress: number,
-      isExistsAddress: number,
-      succeededAddress: number
-    ): Promise<void> {
-      const path = arrayBufferToStringAtIndex(memoryBufferUint8, pathAddress);
-
-      try {
-        const isExists = await storage.getItem(path);
-        memoryBufferUint8[isExistsAddress] = (isExists === undefined || isExists === null) ? 0 : 1;
-        memoryBufferInt32[
-          succeededAddress / Int32Array.BYTES_PER_ELEMENT
-        ] = 1;
-      } catch (error) {
-        memoryBufferInt32[
-          succeededAddress / Int32Array.BYTES_PER_ELEMENT
-        ] = 0;
-      }
-    };
-
-    const pvFileDeleteWasm = async function (
-      pathAddress: number,
-      succeededAddress: number
-    ): Promise<void> {
-      const path = arrayBufferToStringAtIndex(memoryBufferUint8, pathAddress);
-      try {
-        await storage.removeItem(path);
-        memoryBufferInt32[
-          succeededAddress / Int32Array.BYTES_PER_ELEMENT
-        ] = 1;
-      } catch (error) {
-        memoryBufferInt32[
-          succeededAddress / Int32Array.BYTES_PER_ELEMENT
-        ] = 0;
-      }
-    };
-
-    const pvGetBrowserInfo = async function (browserInfoAddressAddress: number): Promise<void> {
-      const userAgent =
-        navigator.userAgent !== undefined ? navigator.userAgent : 'unknown';
-      // eslint-disable-next-line
-      const browserInfoAddress = await aligned_alloc(
-        Uint8Array.BYTES_PER_ELEMENT,
-        (userAgent.length + 1) * Uint8Array.BYTES_PER_ELEMENT
-      );
-
-      if (browserInfoAddress === 0) {
-        throw new Error('malloc failed: Cannot allocate memory');
-      }
-
-      memoryBufferInt32[
-        browserInfoAddressAddress / Int32Array.BYTES_PER_ELEMENT
-      ] = browserInfoAddress;
-      for (let i = 0; i < userAgent.length; i++) {
-        memoryBufferUint8[browserInfoAddress + i] = userAgent.charCodeAt(i);
-      }
-      memoryBufferUint8[browserInfoAddress + userAgent.length] = 0;
-    };
-
-    const pvGetOriginInfo = async function(originInfoAddressAddress: number): Promise<void> {
-      const origin = self.origin ?? self.location.origin;
-      // eslint-disable-next-line
-      const originInfoAddress = await aligned_alloc(
-        Uint8Array.BYTES_PER_ELEMENT,
-        (origin.length + 1) * Uint8Array.BYTES_PER_ELEMENT
-      );
-
-      if (originInfoAddress === 0) {
-        throw new Error('malloc failed: Cannot allocate memory');
-      }
-
-      memoryBufferInt32[
-        originInfoAddressAddress / Int32Array.BYTES_PER_ELEMENT
-      ] = originInfoAddress;
-      for (let i = 0; i < origin.length; i++) {
-        memoryBufferUint8[originInfoAddress + i] = origin.charCodeAt(i);
-      }
-      memoryBufferUint8[originInfoAddress + origin.length] = 0;
-    };
-
-    const importObject = {
-      // eslint-disable-next-line camelcase
-      wasi_snapshot_preview1: wasiSnapshotPreview1Emulator,
-      env: {
-        memory: memory,
-        // eslint-disable-next-line camelcase
-        pv_console_log_wasm: pvConsoleLogWasm,
-        // eslint-disable-next-line camelcase
-        pv_assert_wasm: pvAssertWasm,
-        // eslint-disable-next-line camelcase
-        pv_time_wasm: pvTimeWasm,
-        // eslint-disable-next-line camelcase
-        pv_https_request_wasm: pvHttpsRequestWasm,
-        // eslint-disable-next-line camelcase
-        pv_file_load_wasm: pvFileLoadWasm,
-        // eslint-disable-next-line camelcase
-        pv_file_save_wasm: pvFileSaveWasm,
-        // eslint-disable-next-line camelcase
-        pv_file_exists_wasm: pvFileExistsWasm,
-        // eslint-disable-next-line camelcase
-        pv_file_delete_wasm: pvFileDeleteWasm,
-        // eslint-disable-next-line camelcase
-        pv_get_browser_info: pvGetBrowserInfo,
-        // eslint-disable-next-line camelcase
-        pv_get_origin_info: pvGetOriginInfo,
-      },
-    };
-
-    const wasmCodeArray = base64ToUint8Array(PORCUPINE_WASM_BASE64);
-    const { instance } = await Asyncify.instantiate(
-      wasmCodeArray,
-      importObject
-    );
-
-    const aligned_alloc = instance.exports.aligned_alloc as aligned_alloc_type;
-    const pv_porcupine_version = instance.exports
-      .pv_porcupine_version as pv_porcupine_version_type;
-    const pv_porcupine_frame_length = instance.exports
-      .pv_porcupine_frame_length as pv_porcupine_frame_length_type;
-    const pv_porcupine_process = instance.exports
-      .pv_porcupine_process as pv_porcupine_process_type;
-    const pv_porcupine_delete = instance.exports
-      .pv_porcupine_delete as pv_porcupine_delete_type;
-    const pv_porcupine_init = instance.exports.pv_porcupine_init as pv_porcupine_init_type;
-    const pv_status_to_string = instance.exports
-      .pv_status_to_string as pv_status_to_string_type;
-    const pv_sample_rate = instance.exports.pv_sample_rate as pv_sample_rate_type;
+    const aligned_alloc = exports.aligned_alloc as aligned_alloc_type;
+    const pv_porcupine_version = exports.pv_porcupine_version as pv_porcupine_version_type;
+    const pv_porcupine_frame_length = exports.pv_porcupine_frame_length as pv_porcupine_frame_length_type;
+    const pv_porcupine_process = exports.pv_porcupine_process as pv_porcupine_process_type;
+    const pv_porcupine_delete = exports.pv_porcupine_delete as pv_porcupine_delete_type;
+    const pv_porcupine_init = exports.pv_porcupine_init as pv_porcupine_init_type;
+    const pv_status_to_string = exports.pv_status_to_string as pv_status_to_string_type;
+    const pv_sample_rate = exports.pv_sample_rate as pv_sample_rate_type;
     const keywordIndexAddress = await aligned_alloc(
       Int32Array.BYTES_PER_ELEMENT,
       Int32Array.BYTES_PER_ELEMENT
