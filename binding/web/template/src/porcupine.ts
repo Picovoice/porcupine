ac291b68cf05ed2c91cc79ae64159173d6b3fca1
Pouya Rostam <pouya@picovoice.ai>
2022-03-01 09:36:56 -0800

    rev 1

--- binding/web/template/src/porcupine.ts
+++ binding/web/template/src/porcupine.ts
@@ -328,9 +328,9 @@ export class Porcupine implements PorcupineEngine {
     keywordModels: ArrayLike<Uint8Array>,
     keywordModelSizes: Int32Array,
     sensitivities: Float32Array): Promise<any> {
-    // A WebAssembly page has a constant size of 64KiB. -> 6MiB ~= 100 pages
+    // A WebAssembly page has a constant size of 64KiB. -> 8MiB ~= 128 pages
     // minimum memory requirements for init: 17 pages
-    const memory = new WebAssembly.Memory({ initial: 100, maximum: 300 });
+    const memory = new WebAssembly.Memory({ initial: 128, maximum: 512 });
 
     const memoryBufferUint8 = new Uint8Array(memory.buffer);
     const memoryBufferInt32 = new Int32Array(memory.buffer);
