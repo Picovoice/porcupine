b64f77864883f08cbdeef767a50a929693ae12bc
Pouya Rostam <pouya@picovoice.ai>
2022-03-10 17:57:08 -0800

    init

--- binding/rust/tests/porcupine_tests.rs
+++ binding/rust/tests/porcupine_tests.rs
@@ -9,39 +9,6 @@
     specific language governing permissions and limitations under the License.
 */
 
-mod platform {
-    #[cfg(target_os = "macos")]
-    pub fn pv_platform() -> String {
-        String::from("mac")
-    }
-
-    #[cfg(target_os = "windows")]
-    pub fn pv_platform() -> String {
-        String::from("windows")
-    }
-
-    #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
-    pub fn pv_platform() -> String {
-        String::from("linux")
-    }
-
-    #[cfg(all(target_os = "linux", any(target_arch = "arm", target_arch = "aarch64")))]
-    pub fn pv_platform() -> String {
-        const RPI_MACHINES: [&str; 4] = ["arm11", "cortex-a7", "cortex-a53", "cortex-a72"];
-        const JETSON_MACHINES: [&str; 1] = ["cortex-a57"];
-
-        let machine = find_machine_type();
-        match machine.as_str() {
-            machine if RPI_MACHINES.contains(&machine) => String::from("raspberry-pi"),
-            machine if JETSON_MACHINES.contains(&machine) => String::from("jetson"),
-            "beaglebone" => String::from("beaglebone"),
-            _ => {
-                panic!("ERROR: Please be advised that this device is not officially supported by Picovoice");
-            }
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use itertools::Itertools;
@@ -50,11 +17,74 @@ mod tests {
     use std::fs::File;
     use std::io::BufReader;
 
-    use super::platform::pv_platform;
+    use porcupine::util::pv_platform;
     use porcupine::{BuiltinKeywords, PorcupineBuilder};
 
+    fn append_lang(path:&str, language:&str) -> String{
+        if language == "en" {
+            String::from(path)
+        } else {
+            format!("{}_{}", path, language)
+        }
+    }
+
+    fn model_path_by_language(language:&str) -> String{
+        format!(
+            "{}{}{}", 
+            env!("CARGO_MANIFEST_DIR"), 
+            append_lang("/../../lib/common/porcupine_params", language), 
+            ".pv")
+    }
+
+    fn keyword_path_by_language(keyword_file:&str, language:&str) -> String{
+        format!(
+            "{}{}/{}/{}_{}.ppn", 
+            env!("CARGO_MANIFEST_DIR"), 
+            append_lang("/../../resources/keyword_files", language),
+            pv_platform(),
+            keyword_file,
+            pv_platform()
+        )
+    }
+
+    fn run_porcupine_test(language:&str, keywords:&[&str], ground_truth:&[i32], audio_file_name:&str) {
+        let access_key = env::var("PV_ACCESS_KEY")
+            .expect("Pass the AccessKey in using the PV_ACCESS_KEY env variable");
+        
+        let mut vec = Vec::new();
+        for keyword in keywords.iter() {
+            vec.push(keyword_path_by_language(keyword, language));
+        }
+
+        let porcupine =
+            PorcupineBuilder::new_with_keyword_paths(access_key, &vec)
+                .model_path(model_path_by_language(language))
+                .init()
+                .expect("Unable to create Porcupine");
+
+        let soundfile_path = format!("{}{}{}", env!("CARGO_MANIFEST_DIR"), "/../../resources/audio_samples/", audio_file_name);
+
+        let soundfile = BufReader::new(File::open(&soundfile_path).expect(&soundfile_path));
+        let source = Decoder::new(soundfile).unwrap();
+
+        assert_eq!(porcupine.sample_rate(), source.sample_rate());
+
+        let mut results = Vec::new();
+        for frame in &source.chunks(porcupine.frame_length() as usize) {
+            let frame = frame.collect_vec();
+            if frame.len() == porcupine.frame_length() as usize {
+                let keyword_index = porcupine.process(&frame).unwrap();
+                if keyword_index >= 0 {
+                    results.push(keyword_index);
+                }
+            }
+        }
+
+        assert_eq!(results, ground_truth);
+    }
+
     #[test]
-    fn test_process() {
+    fn test_process_single_builtin() {
         let access_key = env::var("PV_ACCESS_KEY")
             .expect("Pass the AccessKey in using the PV_ACCESS_KEY env variable");
         let porcupine =
@@ -86,7 +116,7 @@ mod tests {
     }
 
     #[test]
-    fn test_process_multiple() {
+    fn test_process_multiple_builtin() {
         let selected_keywords = vec![
             BuiltinKeywords::Americano,
             BuiltinKeywords::Blueberry,
@@ -147,42 +177,96 @@ mod tests {
     }
 
     #[test]
-    fn test_with_non_ascii_model_name() {
-        let access_key = env::var("PV_ACCESS_KEY")
-            .expect("Pass the AccessKey in using the PV_ACCESS_KEY env variable");
-        let keyword_path = format!(
-            "../../resources/keyword_files_es/{}/murciélago_{}.ppn",
-            pv_platform(),
-            pv_platform(),
+    fn test_single_keyword() {
+        run_porcupine_test(
+            "en",
+            &["porcupine"],
+            &[0],
+            "porcupine.wav"
         );
-        let porcupine = PorcupineBuilder::new_with_keyword_paths(access_key, &[keyword_path])
-            .model_path("../../lib/common/porcupine_params_es.pv")
-            .init()
-            .expect("Unable to create Porcupine");
-
-        let soundfile_path = concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/../../resources/audio_samples/murciélago.wav"
-        );
-
-        let soundfile = BufReader::new(File::open(soundfile_path).expect(soundfile_path));
-        let source = Decoder::new(soundfile).unwrap();
-
-        assert_eq!(porcupine.sample_rate(), source.sample_rate());
-
-        let mut results = Vec::new();
-        for frame in &source.chunks(porcupine.frame_length() as usize) {
-            let frame = frame.collect_vec();
-            if frame.len() == porcupine.frame_length() as usize {
-                let keyword_index = porcupine.process(&frame).unwrap();
-                if keyword_index >= 0 {
-                    results.push(keyword_index);
-                }
-            }
-        }
-
-        let expected_results = vec![0, 0];
-
-        assert_eq!(results, expected_results);
     }
+
+    #[test]
+    fn test_multiple_keywords() {
+        run_porcupine_test(
+            "en",
+            &[
+                "americano", "blueberry", "bumblebee", "grapefruit",
+                "grasshopper", "picovoice", "porcupine", "terminator"
+            ],
+            &[6, 0, 1, 2, 3, 4, 5, 6, 7],
+            "multiple_keywords.wav"
+        );
+    }
+
+    #[test]
+    fn test_single_keyword_es() {
+        run_porcupine_test(
+            "es",
+            &["murciélago"],
+            &[0, 0],
+            "murciélago.wav"
+        );
+    }
+
+    #[test]
+    fn test_multiple_keywords_es() {
+        run_porcupine_test(
+            "es",
+            &["emparedado", "leopardo", "manzana"],
+            &[0, 1, 2],
+            "multiple_keywords_es.wav"
+        );
+    }
+
+    #[test]
+    fn test_single_keyword_de() {
+        run_porcupine_test(
+            "de",
+            &["heuschrecke"],
+            &[0],
+            "heuschrecke.wav"
+        );
+    }
+
+    #[test]
+    fn test_multiple_keywords_de() {
+        run_porcupine_test(
+            "de",
+            &["ananas", "heuschrecke", "leguan", "stachelschwein"],
+            &[0, 1, 2, 3],
+            "multiple_keywords_de.wav"
+        );
+    }
+
+    #[test]
+    fn test_single_keyword_fr() {
+        run_porcupine_test(
+            "fr",
+            &["mon chouchou"],
+            &[0],
+            "mon_chouchou.wav"
+        );
+    }
+
+    #[test]
+    fn test_multiple_keywords_fr() {
+        run_porcupine_test(
+            "fr",
+            &["framboise", "mon chouchou", "parapluie"],
+            &[0, 1, 0, 2],
+            "multiple_keywords_fr.wav"
+        );
+    }
+
+    #[test]
+    fn test_with_non_ascii_model_name() {
+        run_porcupine_test(
+            "es",
+            &["murciélago"],
+            &[0, 0],
+            "murciélago.wav"
+        );
+    }
+
 }
