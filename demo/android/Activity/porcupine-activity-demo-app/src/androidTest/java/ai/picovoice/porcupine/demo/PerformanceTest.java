2fb22990a09696ebd64b056bdab5edb49f58fd3a
Kwangsoo Yeo <kyeo@picovoice.ai>
2022-04-07 15:26:50 -0700

    fix android test

--- demo/android/Activity/porcupine-activity-demo-app/src/androidTest/java/ai/picovoice/porcupine/demo/PerformanceTest.java
+++ demo/android/Activity/porcupine-activity-demo-app/src/androidTest/java/ai/picovoice/porcupine/demo/PerformanceTest.java
@@ -18,8 +18,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
@@ -42,10 +48,11 @@ public class PerformanceTest {
     }
 
     @Before
-    public void Setup() {
+    public void Setup() throws IOException {
         testContext = InstrumentationRegistry.getInstrumentation().getContext();
         appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assetManager = testContext.getAssets();
+        extractAssetsRecursively("test_resources");
         testResourcesPath = new File(appContext.getFilesDir(), "test_resources").getAbsolutePath();
 
         accessKey = appContext.getString(R.string.pvTestingAccessKey);
@@ -102,5 +109,36 @@ public class PerformanceTest {
                 String.format("Expected threshold (%.3fs), process took (%.3fs)", performanceThresholdSec, avgSec),
                 avgSec <= performanceThresholdSec
         );
+    }
+
+    private void extractAssetsRecursively(String path) throws IOException {
+        String[] list = assetManager.list(path);
+        if (list.length > 0) {
+            File outputFile = new File(appContext.getFilesDir(), path);
+            if (!outputFile.exists()) {
+                outputFile.mkdirs();
+            }
+
+            for (String file : list) {
+                String filepath = path + "/" + file;
+                extractAssetsRecursively(filepath);
+            }
+        } else {
+            extractTestFile(path);
+        }
+    }
+
+    private void extractTestFile(String filepath) throws IOException {
+        InputStream is = new BufferedInputStream(assetManager.open(filepath), 256);
+        File absPath = new File(appContext.getFilesDir(), filepath);
+        OutputStream os = new BufferedOutputStream(new FileOutputStream(absPath), 256);
+        int r;
+        while ((r = is.read()) != -1) {
+            os.write(r);
+        }
+        os.flush();
+
+        is.close();
+        os.close();
     }
 }
