e70f0591ce5d4f809d0734b950f8b4703eed8807
Pouya Rostam <pouya@picovoice.ai>
2022-05-12 11:54:38 -0700

    released python bindings

--- demo/respeaker/setup.py
+++ demo/respeaker/setup.py
@@ -25,7 +25,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
 
 setuptools.setup(
     name="ppnrespeakerdemo",
-    version="2.1.1",
+    version="2.1.2",
     author="Picovoice Inc.",
     author_email="hello@picovoice.ai",
     description="Porcupine wake word engine demo for ReSpeaker 4-mic hat.",
@@ -33,7 +33,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Picovoice/porcupine",
     packages=["ppnrespeakerdemo"],
-    install_requires=["pvporcupine==2.1.1", "spidev", "gpiozero", "pvrecorder==1.0.2"],
+    install_requires=["pvporcupine==2.1.3", "spidev", "gpiozero", "pvrecorder==1.1.0"],
     include_package_data=True,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
