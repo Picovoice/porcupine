3ea213112ccd4440d8baa32ee19289ed4b9f7ff3
Ian Lavery <ian@picovoice.ai>
2022-06-30 11:16:07 -0700

    tests

--- binding/ios/PorcupineAppTest/PorcupineAppTestUITests/PorcupineAppTestUITests.swift
+++ binding/ios/PorcupineAppTest/PorcupineAppTestUITests/PorcupineAppTestUITests.swift
@@ -12,17 +12,7 @@ import XCTest
 
 import Porcupine
 
-class PorcupineAppTestUITests: XCTestCase {
-    let accessKey: String = "{TESTING_ACCESS_KEY_HERE}"
-    
-    override func setUp() {
-        super.setUp()
-        continueAfterFailure = false
-    }
-
-    override func tearDown() {
-       super.tearDown()
-   }
+class PorcupineAppTestUITests: BaseTest {
 
     func testInitSuccessWithSingleBuiltIn() throws {
         let p = try Porcupine.init(accessKey: accessKey, keyword: Porcupine.BuiltInKeyword.porcupine)
@@ -31,7 +21,7 @@ class PorcupineAppTestUITests: XCTestCase {
         XCTAssert(Porcupine.sampleRate > 0)
         p.delete()
     }
-    
+
     func testInitSuccessWithMultipleBuiltIns() throws {
         let keywords = [
             Porcupine.BuiltInKeyword.alexa,
@@ -39,170 +29,157 @@ class PorcupineAppTestUITests: XCTestCase {
             Porcupine.BuiltInKeyword.picovoice,
             Porcupine.BuiltInKeyword.terminator
         ]
-        
+
         let p = try Porcupine.init(accessKey: accessKey, keywords: keywords)
         p.delete()
     }
-    
+
     func testInitSuccessWithCustomArguments() throws {
         let bundle = Bundle(for: type(of: self))
         let keywordPath = bundle.path(forResource: "hey barista_ios", ofType: "ppn")!
         let modelPath = bundle.path(forResource: "porcupine_params", ofType: "pv")
-        
+
         let p = try Porcupine.init(accessKey: accessKey, keywordPath: keywordPath, modelPath: modelPath)
         p.delete()
     }
-    
+
     func testInitSuccessWithMultipleCustomKeywordsAndSensitivities() throws {
         let bundle = Bundle(for: type(of: self))
         let keywordPath = bundle.path(forResource: "hey barista_ios", ofType: "ppn")!
         let keywordPath2 = bundle.path(forResource: "pico clock_ios", ofType: "ppn")!
-        
+
         let p = try Porcupine.init(accessKey: accessKey, keywordPaths: [keywordPath, keywordPath2], sensitivities: [0.35, 0.6])
         p.delete()
     }
-    
+
     func testInitFailWithMismatchedLanguage() throws {
         let bundle = Bundle(for: type(of: self))
         let keywordPath = bundle.path(forResource: "ananas_ios", ofType: "ppn")!
         let modelPath = bundle.path(forResource: "porcupine_params", ofType: "pv")!
-        
+
         var didFail = false
         do {
             _ = try Porcupine.init(accessKey: accessKey, keywordPath: keywordPath, modelPath: modelPath)
         } catch {
             didFail = true
         }
-        
+
         XCTAssert(didFail)
     }
-    
+
     func testInitFailWithInvalidModelPath() throws {
-        
+
         var didFail = false
         do {
             _ = try Porcupine.init(accessKey: accessKey, keyword: Porcupine.BuiltInKeyword.porcupine, modelPath: "bad_path/bad_path.pv")
         } catch {
             didFail = true
         }
-        
+
         XCTAssert(didFail)
     }
-    
+
     func testInitFailWithInvalidSensitivity() throws {
-        
+
         var didFail = false
         do {
             _ = try Porcupine.init(accessKey: accessKey, keyword: Porcupine.BuiltInKeyword.porcupine, sensitivity: 10)
         } catch {
             didFail = true
         }
-        
+
         XCTAssert(didFail)
     }
-    
+
     func testInitFailWithMismatchedSensitivityCount() throws {
-        
+
         let keywords = [Porcupine.BuiltInKeyword.porcupine, Porcupine.BuiltInKeyword.grasshopper]
-        let sensitivities:[Float32] = [0.3, 0.8, 0.4]
+        let sensitivities: [Float32] = [0.3, 0.8, 0.4]
         var didFail = false
         do {
             _ = try Porcupine.init(accessKey: accessKey, keywords: keywords, sensitivities: sensitivities)
         } catch {
             didFail = true
         }
-        
+
         XCTAssert(didFail)
     }
-    
+
     func testInitFailWithNoKeywords() throws {
-        
-        let keywords:[Porcupine.BuiltInKeyword] = []
+
+        let keywords: [Porcupine.BuiltInKeyword] = []
         var didFail = false
         do {
             _ = try Porcupine.init(accessKey: accessKey, keywords: keywords)
         } catch {
             didFail = true
         }
-        
+
         XCTAssert(didFail)
     }
-    
+
     func testInitFailWithInvalidKeywordPath() throws {
-        
+
         var didFail = false
         do {
             _ = try Porcupine.init(accessKey: accessKey, keywordPath: "bad_path/bad_path.ppn")
         } catch {
             didFail = true
         }
-        
+
         XCTAssert(didFail)
     }
-    
+
     func testInitFailWithWrongKeywordPlatform() throws {
         let bundle = Bundle(for: type(of: self))
         let keywordPath = bundle.path(forResource: "alexa_linux", ofType: "ppn")!
-        
+
         var didFail = false
         do {
             _ = try Porcupine.init(accessKey: accessKey, keywordPath: keywordPath)
         } catch {
             didFail = true
         }
-        
+
         XCTAssert(didFail)
     }
-    
+
     func testInitSuccessES() throws {
         let bundle = Bundle(for: type(of: self))
         let keywordPath = bundle.path(forResource: "emparedado_ios", ofType: "ppn")!
         let modelPath = bundle.path(forResource: "porcupine_params_es", ofType: "pv")!
-        
+
         let p = try Porcupine.init(accessKey: accessKey, keywordPath: keywordPath, modelPath: modelPath)
         p.delete()
     }
-    
+
     func testInitSuccessFR() throws {
         let bundle = Bundle(for: type(of: self))
         let keywordPath = bundle.path(forResource: "framboise_ios", ofType: "ppn")!
         let modelPath = bundle.path(forResource: "porcupine_params_fr", ofType: "pv")!
-        
+
         let p = try Porcupine.init(accessKey: accessKey, keywordPath: keywordPath, modelPath: modelPath)
         p.delete()
     }
-    
+
     func testInitSuccessDE() throws {
         let bundle = Bundle(for: type(of: self))
         let keywordPath = bundle.path(forResource: "ananas_ios", ofType: "ppn")!
         let modelPath = bundle.path(forResource: "porcupine_params_de", ofType: "pv")!
-        
+
         let p = try Porcupine.init(accessKey: accessKey, keywordPath: keywordPath, modelPath: modelPath)
         p.delete()
     }
-    
+
     func testProcSuccessWithSingleBuiltIn() throws {
-        let p:Porcupine = try Porcupine.init(accessKey: accessKey, keyword: Porcupine.BuiltInKeyword.porcupine)
-        
+        let p: Porcupine = try Porcupine.init(accessKey: accessKey, keyword: Porcupine.BuiltInKeyword.porcupine)
+
         let bundle = Bundle(for: type(of: self))
-        let fileURL:URL = bundle.url(forResource: "porcupine", withExtension: "wav")!
-    
-        let data = try Data(contentsOf: fileURL)
-        let frameLengthBytes = Int(Porcupine.frameLength) * 2
-        var pcmBuffer = Array<Int16>(repeating: 0, count: Int(Porcupine.frameLength))
-        var results:[Int32] = []
-        var index = 44
-        while(index + frameLengthBytes < data.count) {
-            _ = pcmBuffer.withUnsafeMutableBytes { data.copyBytes(to: $0, from: index..<(index + frameLengthBytes)) }
-            let keywordIndex:Int32 = try p.process(pcm:pcmBuffer)
-            if(keywordIndex == 0){
-                results.append(keywordIndex)
-            }
-            
-            index += frameLengthBytes
-        }
+        let fileURL: URL = bundle.url(forResource: "porcupine", withExtension: "wav")!
+
+        let results = try processFile(p: p, testAudioURL: fileURL)
         p.delete()
-        
+
         XCTAssert(results.count == 1 && results[0] == 0)
     }
 
@@ -210,7 +187,7 @@ class PorcupineAppTestUITests: XCTestCase {
         let bundle = Bundle(for: type(of: self))
         let keywordPath = bundle.path(forResource: "murciélago_ios", ofType: "ppn")!
         let modelPath = bundle.path(forResource: "porcupine_params_es", ofType: "pv")!
-        
+
         let p = try Porcupine.init(accessKey: accessKey, keywordPath: keywordPath, modelPath: modelPath)
         p.delete()
     }
@@ -228,23 +205,25 @@ class PorcupineAppTestUITests: XCTestCase {
             Porcupine.BuiltInKeyword.terminator
         ]
 
-        let p:Porcupine = try Porcupine.init(accessKey: accessKey, keywords: keywords)
-        
+        let p: Porcupine = try Porcupine.init(accessKey: accessKey, keywords: keywords)
+
         let bundle = Bundle(for: type(of: self))
-        let fileURL:URL = bundle.url(forResource: "multiple_keywords", withExtension: "wav")!
-    
+        let fileURL: URL = bundle.url(forResource: "multiple_keywords", withExtension: "wav")!
+
         let data = try Data(contentsOf: fileURL)
         let frameLengthBytes = Int(Porcupine.frameLength) * 2
         var pcmBuffer = Array<Int16>(repeating: 0, count: Int(Porcupine.frameLength))
-        var results:[Int32] = []
+        var results: [Int32] = []
         var index = 44
-        while(index + frameLengthBytes < data.count) {
-            _ = pcmBuffer.withUnsafeMutableBytes { data.copyBytes(to: $0, from: index..<(index + frameLengthBytes)) }
-            let keywordIndex:Int32 = try p.process(pcm:pcmBuffer)
-            if(keywordIndex >= 0){
+        while (index + frameLengthBytes < data.count) {
+            _ = pcmBuffer.withUnsafeMutableBytes {
+                data.copyBytes(to: $0, from: index..<(index + frameLengthBytes))
+            }
+            let keywordIndex: Int32 = try p.process(pcm: pcmBuffer)
+            if (keywordIndex >= 0) {
                 results.append(keywordIndex)
             }
-            
+
             index += frameLengthBytes
         }
         p.delete()
---
+++ binding/ios/PorcupineAppTest/PorcupineAppTestUITests/PorcupineLanguageTests.swift
@@ -0,0 +1,84 @@
+//
+//  Copyright 2022 Picovoice Inc.
+//  You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
+//  file accompanying this source.
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+//  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+//  specific language governing permissions and limitations under the License.
+//
+
+import AVFoundation
+import XCTest
+
+import Porcupine
+
+class PorcupineLanguageTests: BaseTest {
+
+    static var testData: [[Any]] = [
+        ["en",
+         ["americano", "blueberry", "bumblebee", "grapefruit", "grasshopper", "picovoice", "porcupine", "terminator"],
+         [6, 0, 1, 2, 3, 4, 5, 6, 7]],
+        ["es", ["emparedado", "leopardo", "manzana"], [0, 1, 2]],
+        ["de", ["ananas", "heuschrecke", "leguan", "stachelschwein"], [0, 1, 2, 3]],
+        ["fr", ["framboise", "mon chouchou", "parapluie"], [0, 1, 0, 2]],
+        ["it", ["espresso", "cameriere", "porcospino"], [2, 0, 1]],
+        ["ja", ["ninja", "bushi", "ringo"], [2, 1, 0]],
+        ["ko", ["aiseukeulim", "bigseubi", "koppulso"], [1, 2, 0]],
+        ["pt", ["abacaxi", "fenomeno", "formiga"], [0, 2, 1]],
+    ]
+
+    var language: String = ""
+    var modelPath: String = ""
+    var keywordPaths: [String] = []
+    var testAudioPath: URL? = URL(string: "")
+    var expectedResults: [Int] = []
+
+    override class var defaultTestSuite: XCTestSuite {
+        get {
+            let xcTestSuite = XCTestSuite(name: NSStringFromClass(self))
+            let bundle = Bundle(for: self)
+
+            for testCase in testData {
+                let suffix = (testCase[0]) as! String == "en" ? "" : "_\(testCase[0])"
+                for invocation in testInvocations {
+                    let newTestCase = PorcupineLanguageTests(invocation: invocation)
+
+                    var keywordFiles: [String] = []
+                    for keyword in testCase[1] as! [String] {
+                        keywordFiles.append(bundle.path(forResource: "\(keyword)_ios", ofType: "ppn")!)
+                    }
+
+                    newTestCase.language = testCase[0] as! String
+                    newTestCase.modelPath = bundle.path(forResource: "porcupine_params\(suffix)", ofType: "pv")!
+                    newTestCase.keywordPaths = keywordFiles
+                    newTestCase.testAudioPath = bundle.url(forResource: "multiple_keywords\(suffix)", withExtension: "wav")!
+                    newTestCase.expectedResults = testCase[2] as! [Int]
+                    xcTestSuite.addTest(newTestCase)
+                }
+            }
+
+            return xcTestSuite
+        }
+    }
+
+    func testWrapper() throws {
+        let testResults = try XCTContext.runActivity(named: "(\(language))") { _ -> [Int32] in
+            let p = try Porcupine.init(
+                    accessKey: accessKey,
+                    keywordPaths: keywordPaths,
+                    modelPath: modelPath)
+            XCTAssert(Porcupine.version != "")
+            XCTAssert(Porcupine.frameLength > 0)
+            XCTAssert(Porcupine.sampleRate > 0)
+
+            let ret = try processFile(p: p, testAudioURL: testAudioPath!)
+            p.delete()
+            return ret
+        }
+
+        XCTAssert(expectedResults.count == testResults.count)
+        for i in 0..<expectedResults.count {
+            XCTAssert(expectedResults[i] == testResults[i])
+        }
+    }
+}
