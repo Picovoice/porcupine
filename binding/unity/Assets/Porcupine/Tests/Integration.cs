a9847f1dc950dac23491bf1b066c07d3cfd7c285
Eric Mikulin <eric@picovoice.ai>
2023-01-06 16:57:04 -0800

    Initial Unity

---
+++ binding/unity/Assets/Porcupine/Tests/Integration.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Integration
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void IntegrationSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator IntegrationWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
