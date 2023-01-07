a25f32350e518085d063fd58a606ece2725817a6
Kwangsoo Yeo <kyeo@picovoice.ai>
2023-01-06 17:48:39 -0800

    update test

--- binding/react/test/use_porcupine.test.ts
+++ binding/react/test/use_porcupine.test.ts
@@ -10,68 +10,68 @@ import porcupineParams from '@/porcupine_params.js';
 const ACCESS_KEY = Cypress.env('ACCESS_KEY');
 
 describe('Porcupine binding', () => {
-  it('should be able to init via public path', async () => {
+  it('should be able to init via public path', () => {
     const { result } = renderHook(() => usePorcupine());
 
-    await act(async () => {
-      await result.current.init(
+    cy.wrapHook(
+      () => result.current.init(
         ACCESS_KEY,
         BuiltInKeyword.Porcupine,
         { publicPath: "/test/porcupine_params.pv", forceWrite: true }
-      );
+      )
+    ).then(() => {
+      expect(result.current.isLoaded).to.be.true;
     });
 
-    expect(result.current.isLoaded).to.be.true;
-
-    await act(async () => {
-      await result.current.release();
+    cy.wrapHook(
+      result.current.release
+    ).then(() => {
+      expect(result.current.isLoaded).to.be.false;
     });
-
-    expect(result.current.isLoaded).to.be.false;
   });
 
-  it('should be able to init via base64', async () => {
+  it('should be able to init via base64', () => {
     const { result } = renderHook(() => usePorcupine());
 
-    await act(async () => {
-      await result.current.init(
+    cy.wrapHook(
+      () => result.current.init(
         ACCESS_KEY,
         BuiltInKeyword.Porcupine,
         { base64: porcupineParams, forceWrite: true }
-      );
+      )
+    ).then(() => {
+      expect(result.current.isLoaded).to.be.true;
     });
-
-    expect(result.current.isLoaded).to.be.true;
   });
 
-  it('should show invalid model path error message', async () => {
+  it('should show invalid model path error message', () => {
     const { result } = renderHook(() => usePorcupine());
 
-    await act(async () => {
-      await result.current.init(
+    cy.wrapHook(
+      () => result.current.init(
         ACCESS_KEY,
         BuiltInKeyword.Porcupine,
         { publicPath: "/porcupine_params_failed.pv", forceWrite: true }
-      );
+      )
+    ).then(() => {
+      expect(result.current.isLoaded).to.be.false;
+      expect(result.current.error).to.contain("Error response returned while fetching model from '/porcupine_params_failed.pv'");
     });
-
-    expect(result.current.isLoaded).to.be.false;
-    expect(result.current.error).to.contain("Error response returned while fetching model from '/porcupine_params_failed.pv'");
   });
 
-  it('should show invalid access key error message', async () => {
+  it('should show invalid access key error message', () => {
     const { result } = renderHook(() => usePorcupine());
 
-    await act(async () => {
-      await result.current.init(
+    cy.wrapHook(
+      () => result.current.init(
         '',
         BuiltInKeyword.Porcupine,
         { publicPath: "/test/porcupine_params.pv", forceWrite: true }
-      );
+      )
+    ).then(() => {
+      expect(result.current.isLoaded).to.be.false;
+      expect(result.current.error).to.contain("Invalid AccessKey");
     });
-
-    expect(result.current.isLoaded).to.be.false;
-    expect(result.current.error).to.contain("Invalid AccessKey");
   });
 
   it('should be able to process audio', () => {
