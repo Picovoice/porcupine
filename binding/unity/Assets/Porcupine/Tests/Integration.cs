e0d8468667c0e7570d65ae1a8a474b8687f578a1
Eric Mikulin <eric@picovoice.ai>
2023-01-10 17:01:36 -0800

    Working for iOS

--- binding/unity/Assets/Porcupine/Tests/Integration.cs
+++ binding/unity/Assets/Porcupine/Tests/Integration.cs
@@ -135,7 +135,7 @@ namespace Tests
         private List<short> GetPcmFromFile(string audioFilePath, int expectedSampleRate)
         {
             List<short> data = new List<short>();
-            using (BinaryReader reader = new BinaryReader(File.Open(audioFilePath, FileMode.Open)))
+            using (BinaryReader reader = new BinaryReader(File.OpenRead(audioFilePath)))
             {
                 reader.ReadBytes(24); // skip over part of the header
                 Assert.AreEqual(reader.ReadInt32(), expectedSampleRate, "Specified sample rate did not match test file.");
