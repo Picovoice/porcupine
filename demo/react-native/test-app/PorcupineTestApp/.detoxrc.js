f1fee71efa404ecc6d3bf2e0f3bc0a17114f8f4e
Eric Mikulin <eric@picovoice.ai>
2023-01-18 13:22:31 -0800

    Use correct sims

--- demo/react-native/test-app/PorcupineTestApp/.detoxrc.js
+++ demo/react-native/test-app/PorcupineTestApp/.detoxrc.js
@@ -38,7 +38,7 @@ module.exports = {
     simulator: {
       type: 'ios.simulator',
       device: {
-        type: 'iPhone 12'
+        type: 'iPhone 13'
       }
     },
     attached: {
@@ -50,7 +50,7 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_5_API_30'
+        avdName: 'Pixel_3a_API_33_x86_64'
       }
     }
   },
