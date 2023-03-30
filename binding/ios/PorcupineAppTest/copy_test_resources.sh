6ceabaeccae88f44112eff3da63f829f70aa5934
Eric Mikulin <eric@picovoice.ai>
2023-03-29 17:33:47 -0700

    Initial v2.2 iOS

--- binding/ios/PorcupineAppTest/copy_test_resources.sh
+++ binding/ios/PorcupineAppTest/copy_test_resources.sh
@@ -28,7 +28,7 @@ mkdir -p ${ASSETS_DIR}/model_files
 cp ${LIB_DIR}/common/*.pv ${ASSETS_DIR}/model_files
 
 echo "Copying test data file..."
-cp ${RESOURCE_DIR}/test/test_data.json ${ASSETS_DIR}
+cp ${RESOURCE_DIR}/.test/test_data.json ${ASSETS_DIR}
 
 echo "Fixing filename encodings for Appcenter compatibility"
 convmv --notest -f utf8 -t utf8 --nfd -r ${ASSETS_DIR}
