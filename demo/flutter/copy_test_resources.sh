007d78e98298b3094c72151685ed47cfd974c291
Ian <ian@picovoice.ai>
2023-03-31 16:09:17 -0700

    fix copy script

--- demo/flutter/copy_test_resources.sh
+++ demo/flutter/copy_test_resources.sh
@@ -27,4 +27,4 @@ mkdir -p ${ASSETS_DIR}/model_files
 cp ${LIB_DIR}/common/*.pv ${ASSETS_DIR}/model_files
 
 echo "Copying test data file..."
-cp ${RESOURCE_DIR}/test/test_data.json ${ASSETS_DIR}
+cp ${RESOURCE_DIR}/.test/test_data.json ${ASSETS_DIR}
