748ecf9c67f36ba05796777788946f7d21ea2c02
Ian Lavery <ian@picovoice.ai>
2023-05-09 13:35:13 -0700

    Multilingual iOS demo (#967)

---
+++ demo/ios/ForegroundApp/pre-build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mkdir -p "${SRCROOT}/PorcupineForegroundAppDemo/keywords/"
+mkdir -p "${SRCROOT}/PorcupineForegroundAppDemo/models/"
+
+rm "${SRCROOT}/PorcupineForegroundAppDemo/keywords/"*
+rm "${SRCROOT}/PorcupineForegroundAppDemo/models/"*
+
+if [ $1 == 'en' ];
+then
+    SUFFIX=""
+else
+    cp "${SRCROOT}/../../../resources/keyword_files_$1/ios/"* "${SRCROOT}/PorcupineForegroundAppDemo/keywords/"
+    cp "${SRCROOT}/../../../lib/common/porcupine_params_$1.pv" "${SRCROOT}/PorcupineForegroundAppDemo/models/"
+fi
