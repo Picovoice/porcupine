9c9760c480a816e70a74c680417ff793eff1b202
Ian Lavery <ian@picovoice.ai>
2023-05-11 17:09:34 -0700

    Update pre-build.sh

--- demo/ios/ForegroundApp/pre-build.sh
+++ demo/ios/ForegroundApp/pre-build.sh
@@ -6,10 +6,8 @@ mkdir -p "${SRCROOT}/PorcupineForegroundAppDemo/models/"
 rm "${SRCROOT}/PorcupineForegroundAppDemo/keywords/"*
 rm "${SRCROOT}/PorcupineForegroundAppDemo/models/"*
 
-if [ $1 == 'en' ];
+if [ $1 != 'en' ];
 then
-    SUFFIX=""
-else
     cp "${SRCROOT}/../../../resources/keyword_files_$1/ios/"* "${SRCROOT}/PorcupineForegroundAppDemo/keywords/"
     cp "${SRCROOT}/../../../lib/common/porcupine_params_$1.pv" "${SRCROOT}/PorcupineForegroundAppDemo/models/"
 fi
