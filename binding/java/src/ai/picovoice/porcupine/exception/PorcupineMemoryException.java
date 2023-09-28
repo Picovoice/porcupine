a5b255805f482db8d9d2514a44ba84023ff4e1a3
Kwangsoo Yeo <kyeo@picovoice.ai>
2023-09-28 16:30:55 -0700

    update exceptions

--- binding/java/src/ai/picovoice/porcupine/exception/PorcupineMemoryException.java
+++ binding/java/src/ai/picovoice/porcupine/exception/PorcupineMemoryException.java
@@ -1,5 +1,5 @@
 /*
-    Copyright 2021 Picovoice Inc.
+    Copyright 2021-2023 Picovoice Inc.
     You may not use this file except in compliance with the license. A copy of the license is
     located in the "LICENSE" file accompanying this source.
     Unless required by applicable law or agreed to in writing, software distributed under the
