8413ad4844d7845ea0c32f52c591d78accaeb05c
Ian <ian@picovoice.ai>
2023-10-05 21:10:29 -0700

    libs

--- lib/ios/PvPorcupine.xcframework/ios-arm64/PvPorcupine.framework/Headers/picovoice.h
+++ lib/ios/PvPorcupine.xcframework/ios-arm64/PvPorcupine.framework/Headers/picovoice.h
@@ -1,14 +1,12 @@
 /*
     Copyright 2018-2023 Picovoice Inc.
 
-    You may not use this file except in compliance with the license. A copy of
-   the license is located in the "LICENSE" file accompanying this source.
+    You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
+    file accompanying this source.
 
-    Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-   License for the specific language governing permissions and limitations under
-   the License.
+    Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+    an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+    specific language governing permissions and limitations under the License.
 */
 
 #ifndef PICOVOICE_H
@@ -54,6 +52,30 @@ typedef enum {
  * @return String representation.
  */
 PV_API const char *pv_status_to_string(pv_status_t status);
+
+/**
+ * If a function returns a failure (any pv_status_t other than PV_STATUS_SUCCESS), this function can be called
+ * to get a series of error messages related to the failure. This function can only be called only once per
+ * failure status on another function. The memory for `message_stack` must be freed using `pv_free_error_stack`.
+ *
+ * Regardless of the return status of this function, if `message_stack` is not `NULL`, then `message_stack`
+ * contains valid memory. However, a failure status on this function indicates that future error messages
+ * may not be reported.
+ *
+ * @param[out] message_stack Array of messages relating to the failure. Messages are NULL terminated strings.
+ *                           The array and messages must be freed using `pv_free_error_stack`.
+ * @param[out] message_stack_depth The number of messages in the `message_stack` array.
+ */
+PV_API pv_status_t pv_get_error_stack(
+        char ***message_stack,
+        int32_t *message_stack_depth);
+
+/**
+ * This function frees the memory used by error messages allocated by `pv_get_error_stack`.
+ *
+ * @param message_stack Array of messages relating to the failure, allocated from `pv_get_error_stack`.
+ */
+PV_API void pv_free_error_stack(char **message_stack);
 
 #ifdef __cplusplus
 }
--- lib/ios/PvPorcupine.xcframework/ios-arm64/PvPorcupine.framework/Headers/pv_porcupine.h
+++ lib/ios/PvPorcupine.xcframework/ios-arm64/PvPorcupine.framework/Headers/pv_porcupine.h
@@ -1,14 +1,12 @@
 /*
     Copyright 2018-2023 Picovoice Inc.
 
-    You may not use this file except in compliance with the license. A copy of
-   the license is located in the "LICENSE" file accompanying this source.
+    You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
+    file accompanying this source.
 
-    Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-   License for the specific language governing permissions and limitations under
-   the License.
+    Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+    an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+    specific language governing permissions and limitations under the License.
 */
 
 #ifndef PV_PORCUPINE_H
@@ -26,36 +24,31 @@ extern "C" {
 #endif
 
 /**
- * Forward declaration for Porcupine wake word engine. It detects utterances of
- * given keywords within an incoming stream of audio in real-time. It processes
- * incoming audio in consecutive frames and for each frame emits the detection
- * result. The number of samples per frame can be attained by calling
- * 'pv_porcupine_frame_length()'. The incoming audio needs to have a sample rate
- * equal to 'pv_sample_rate()' and be 16-bit linearly-encoded. Porcupine
- * operates on single-channel audio.
+ * Forward declaration for Porcupine wake word engine. It detects utterances of given keywords within an incoming stream
+ * of audio in real-time. It processes incoming audio in consecutive frames and for each frame emits the detection
+ * result. The number of samples per frame can be attained by calling 'pv_porcupine_frame_length()'. The incoming audio
+ * needs to have a sample rate equal to 'pv_sample_rate()' and be 16-bit linearly-encoded. Porcupine operates on
+ * single-channel audio.
  */
 typedef struct pv_porcupine pv_porcupine_t;
 
 /**
  * Constructor.
  *
- * @param access_key AccessKey obtained from Picovoice Console
- * (https://console.picovoice.ai/).
- * @param model_path Absolute path to the file containing model parameters.
+ * @param access_key AccessKey obtained from Picovoice Console (https://console.picovoice.ai/).
  * @param num_keywords Number of keywords to monitor.
- * @param keyword_paths Absolute paths to keyword model files.
- * @param sensitivities Sensitivities for detecting keywords. Each value should
- * be a number within [0, 1]. A higher sensitivity results in fewer misses at
- * the cost of increasing the false alarm rate.
+ * @param keyword_model_sizes Size of each keyword model in bytes.
+ * @param keyword_models Keyword models.
+ * @param sensitivities Sensitivities for detecting keywords. Each value should be a number within [0, 1]. A higher
+ * sensitivity results in fewer misses at the cost of increasing the false alarm rate.
  * @param[out] object Constructed instance of Porcupine.
- * @return Status code. Returns 'PV_STATUS_INVALID_ARGUMENT',
- * 'PV_STATUS_IO_ERROR', or 'PV_STATUS_OUT_OF_MEMORY' on failure.
+ * @return Status code. Returns 'PV_STATUS_INVALID_ARGUMENT' or 'PV_STATUS_OUT_OF_MEMORY' on failure.
  */
 PV_API pv_status_t pv_porcupine_init(
         const char *access_key,
-        const char *model_path,
         int32_t num_keywords,
-        const char *const *keyword_paths,
+        const int32_t *keyword_model_sizes,
+        const void *const *keyword_models,
         const float *sensitivities,
         pv_porcupine_t **object);
 
@@ -67,20 +60,15 @@ PV_API pv_status_t pv_porcupine_init(
 PV_API void pv_porcupine_delete(pv_porcupine_t *object);
 
 /**
- * Processes a frame of the incoming audio stream and emits the detection
- * result.
+ * Processes a frame of the incoming audio stream and emits the detection result.
  *
  * @param object Porcupine object.
- * @param pcm A frame of audio samples. The number of samples per frame can be
- * attained by calling 'pv_porcupine_frame_length()'. The incoming audio needs
- * to have a sample rate equal to 'pv_sample_rate()' and be 16-bit
- * linearly-encoded. Porcupine operates on single-channel audio.
- * @param[out] keyword_index Index of observed keyword at the end of the current
- * frame. Indexing is 0-based and matches the ordering of keyword models
- * provided to 'pv_porcupine_init()'. If no keyword is detected then it is set
- * to -1.
- * @return Status code. Returns 'PV_STATUS_INVALID_ARGUMENT' or
- * 'PV_STATUS_OUT_OF_MEMORY' on failure.
+ * @param pcm A frame of audio samples. The number of samples per frame can be attained by calling
+ * 'pv_porcupine_frame_length()'. The incoming audio needs to have a sample rate equal to 'pv_sample_rate()' and be
+ * 16-bit linearly-encoded. Porcupine operates on single-channel audio.
+ * @param[out] keyword_index Index of observed keyword at the end of the current frame. Indexing is 0-based and matches
+ * the ordering of keyword models provided to 'pv_porcupine_init()'. If no keyword is detected then it is set to -1.
+ * @return Status code. Returns 'PV_STATUS_INVALID_ARGUMENT' or 'PV_STATUS_OUT_OF_MEMORY' on failure.
  */
 PV_API pv_status_t pv_porcupine_process(pv_porcupine_t *object, const int16_t *pcm, int32_t *keyword_index);
 
