49bcce688cf4b453e190c7c4615663e0f3d9ebf3
Pouya Rostam <pouya@picovoice.ai>
2023-10-06 16:39:35 -0700

    wip

--- demo/mcu/stm32f769/stm32f769i-disco/Src/main.c
+++ demo/mcu/stm32f769/stm32f769i-disco/Src/main.c
@@ -43,7 +43,7 @@ static void error_handler(void) {
 
 void print_error_message(char **message_stack, int32_t message_stack_depth) {
     for (int32_t i = 0; i < message_stack_depth; i++) {
-        printf("\n  [%d] %s", i, message_stack[i]);
+        printf("[%ld] %s\n", i, message_stack[i]);
     }
 }
 
@@ -87,20 +87,15 @@ int main(void) {
     status = pv_porcupine_init(ACCESS_KEY, MEMORY_BUFFER_SIZE, memory_buffer, 1, &KEYWORD_MODEL_SIZES, &KEYWORD_MODELS, &SENSITIVITY, &handle);
 
     if (status != PV_STATUS_SUCCESS) {
-        printf("Porcupine init failed with '%s'", pv_status_to_string(status));
+        printf("Porcupine init failed with '%s':\n", pv_status_to_string(status));
 
         error_status = pv_get_error_stack(&message_stack, &message_stack_depth);
         if (error_status != PV_STATUS_SUCCESS) {
-            printf(".\nUnable to get Porcupine error state with '%s'.\n", pv_status_to_string(error_status));
+            printf("Unable to get Porcupine error state with '%s':\n", pv_status_to_string(error_status));
             error_handler();
         }
 
-        if (message_stack_depth > 0) {
-            printf(":\n");
-            print_error_message(message_stack, message_stack_depth);
-        } else {
-            printf(".\n");
-        }
+        print_error_message(message_stack, message_stack_depth);
         pv_free_error_stack(message_stack);
 
         error_handler();
