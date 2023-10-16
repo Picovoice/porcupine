742fbdaac23cca31df54fc2bc3c604e056dea2e8
Kwangsoo Yeo <kyeo@picovoice.ai>
2023-10-16 11:24:54 -0700

    fix build

--- binding/unity/Assets/Porcupine/Porcupine.cs
+++ binding/unity/Assets/Porcupine/Porcupine.cs
@@ -100,7 +100,7 @@ namespace Pv.Unity
         private static extern void pv_set_sdk(string sdk);
 
         [DllImport(LIBRARY_PATH, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
-        private static extern void pv_get_error_stack(out IntPtr messageStack, out int messageStackDepth);
+        private static extern PorcupineStatus pv_get_error_stack(out IntPtr messageStack, out int messageStackDepth);
 
         [DllImport(LIBRARY_PATH, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
         private static extern void pv_free_error_stack(IntPtr messageStack);
