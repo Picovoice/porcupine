b4b71fbe670e319d0129a4367b45b80ca44a31df
Ian <ian@picovoice.ai>
2024-01-30 17:08:07 -0800

    fix

--- binding/dotnet/PorcupineTest/MainTest.cs
+++ binding/dotnet/PorcupineTest/MainTest.cs
@@ -57,7 +57,8 @@ namespace PorcupineTest
             {
                 case "0xc07":
                 case "0xd03":
-                case "0xd08": return "raspberry-pi";
+                case "0xd08":
+                case "0xd0b": return "raspberry-pi";
                 case "0xd07": return "jetson";
                 case "0xc08": return "beaglebone";
                 default:
