2e25909821531286207cd9badcbc5962035426c3
Ian Lavery <ian@picovoice.ai>
2024-10-24 13:03:58 -0700

    add res

--- Package.swift
+++ Package.swift
@@ -31,23 +31,23 @@ let package = Package(
             ],
             path: "binding/ios",
             exclude: ["PorcupineAppTest"]
-            // resources: [
-            //     .process("resources/keyword_files/ios/alexa_ios.ppn"),
-            //     .process("resources/keyword_files/ios/americano_ios.ppn"),
-            //     .process("resources/keyword_files/ios/blueberry_ios.ppn"),
-            //     .process("resources/keyword_files/ios/bumblebee_ios.ppn"),
-            //     .process("resources/keyword_files/ios/computer_ios.ppn"),
-            //     .process("resources/keyword_files/ios/grapefruit_ios.ppn"),
-            //     .process("resources/keyword_files/ios/grasshopper_ios.ppn"),
-            //     .process("resources/keyword_files/ios/hey google_ios.ppn"),
-            //     .process("resources/keyword_files/ios/hey siri_ios.ppn"),
-            //     .process("resources/keyword_files/ios/jarvis_ios.ppn"),
-            //     .process("resources/keyword_files/ios/ok google_ios.ppn"),
-            //     .process("resources/keyword_files/ios/picovoice_ios.ppn"),
-            //     .process("resources/keyword_files/ios/porcupine_ios.ppn"),
-            //     .process("resources/keyword_files/ios/terminator_ios.ppn"),
-            //     .process("lib/common/porcupine_params.pv")
-            // ]
+            resources: [
+                .process("resources/keyword_files/ios/alexa_ios.ppn"),
+                .process("resources/keyword_files/ios/americano_ios.ppn"),
+                .process("resources/keyword_files/ios/blueberry_ios.ppn"),
+                .process("resources/keyword_files/ios/bumblebee_ios.ppn"),
+                .process("resources/keyword_files/ios/computer_ios.ppn"),
+                .process("resources/keyword_files/ios/grapefruit_ios.ppn"),
+                .process("resources/keyword_files/ios/grasshopper_ios.ppn"),
+                .process("resources/keyword_files/ios/hey\ google_ios.ppn"),
+                .process("resources/keyword_files/ios/hey\ siri_ios.ppn"),
+                .process("resources/keyword_files/ios/jarvis_ios.ppn"),
+                .process("resources/keyword_files/ios/ok\ google_ios.ppn"),
+                .process("resources/keyword_files/ios/picovoice_ios.ppn"),
+                .process("resources/keyword_files/ios/porcupine_ios.ppn"),
+                .process("resources/keyword_files/ios/terminator_ios.ppn"),
+                .process("lib/common/porcupine_params.pv")
+            ]
         )
     ]
 )
