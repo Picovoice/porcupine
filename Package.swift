879f19d9f5e0ed8896d6e179d678685ad4f44599
Ian Lavery <ian@picovoice.ai>
2024-10-23 17:36:31 -0700

    add pkg

---
+++ Package.swift
@@ -0,0 +1,51 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "Porcupine-iOS",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "Porcupine",
+            targets: ["Porcupine"])
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/Picovoice/ios-voice-processor.git",
+            .upToNextMajor(from: "1.1.2")
+        )
+    ],
+    targets: [
+        .target(
+            name: "Porcupine",
+            dependencies: [
+                .product(name: "ios_voice_processor", package: "ios-voice-processor")
+            ],
+            path: "binding/ios",
+            exclude: ["PorcupineAppTest"],
+            resources: [
+                .process("resources/keyword_files/ios/alexa_ios.ppn"),
+                .process("resources/keyword_files/ios/americano_ios.ppn"),
+                .process("resources/keyword_files/ios/blueberry_ios.ppn"),
+                .process("resources/keyword_files/ios/bumblebee_ios.ppn"),
+                .process("resources/keyword_files/ios/computer_ios.ppn"),
+                .process("resources/keyword_files/ios/grapefruit_ios.ppn"),
+                .process("resources/keyword_files/ios/grasshopper_ios.ppn"),
+                .process("resources/keyword_files/ios/hey google_ios.ppn"),
+                .process("resources/keyword_files/ios/hey siri_ios.ppn"),
+                .process("resources/keyword_files/ios/jarvis_ios.ppn"),
+                .process("resources/keyword_files/ios/ok google_ios.ppn"),
+                .process("resources/keyword_files/ios/picovoice_ios.ppn"),
+                .process("resources/keyword_files/ios/porcupine_ios.ppn"),
+                .process("resources/keyword_files/ios/terminator_ios.ppn"),
+                .process("lib/common/porcupine_params.pv")
+            ],
+            linkerSettings: [
+                .linkedFramework("PvPorcupine.xcframework", .path("lib/ios"))
+            ]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
