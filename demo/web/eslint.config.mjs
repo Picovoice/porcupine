374264b748743bdb96625a1bd0a39fa3fa79f303
picoliu <liu@picovoice.ai>
2025-03-17 14:12:35 -0700

    Web demo improvements (#1377)

---
+++ demo/web/eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        porcupineKeywords: "readonly",
+        porcupineModel: "readonly",
+        PorcupineWeb: "readonly",
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": ["off"],
+    },
+  },
+];
