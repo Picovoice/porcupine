e6b07a672871c22b8617f198f8520ee575643865
Zhuojin Liu <liu@picovoice.ai>
2025-04-04 15:12:42 -0700

    fix background permission

--- demo/ios/ForegroundApp/pre-build.sh
+++ demo/ios/ForegroundApp/pre-build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-mkdir -p "${SRCROOT}/PorcupineForegroundAppDemo/keywords/"
-mkdir -p "${SRCROOT}/PorcupineForegroundAppDemo/models/"
+mkdir -p "${SRCROOT}/ForegroundApp/keywords/"
+mkdir -p "${SRCROOT}/ForegroundApp/models/"
 
-rm "${SRCROOT}/PorcupineForegroundAppDemo/keywords/"*
-rm "${SRCROOT}/PorcupineForegroundAppDemo/models/"*
+rm "${SRCROOT}/ForegroundApp/keywords/"*
+rm "${SRCROOT}/ForegroundApp/models/"*
 
 if [ $1 != 'en' ];
 then
-    cp "${SRCROOT}/../../../resources/keyword_files_$1/ios/"* "${SRCROOT}/PorcupineForegroundAppDemo/keywords/"
-    cp "${SRCROOT}/../../../lib/common/porcupine_params_$1.pv" "${SRCROOT}/PorcupineForegroundAppDemo/models/"
+    cp "${SRCROOT}/../../../resources/keyword_files_$1/ios/"* "${SRCROOT}/ForegroundApp/keywords/"
+    cp "${SRCROOT}/../../../lib/common/porcupine_params_$1.pv" "${SRCROOT}/ForegroundApp/models/"
 fi
