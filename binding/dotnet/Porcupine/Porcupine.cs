e4921e40cc1650dbda082940b6680ca0003c33bc
Ian Lavery <ian@picovoice.ai>
2025-12-09 11:25:03 -0800

    fix

--- binding/dotnet/Porcupine/Porcupine.cs
+++ binding/dotnet/Porcupine/Porcupine.cs
@@ -474,7 +474,7 @@ namespace Pv
             Dispose();
         }
 
-        private string[] GetMessageStack()
+        private static string[] GetMessageStack()
         {
             int messageStackDepth;
             IntPtr messageStackRef;
